b3a39e92c25fc2bfa5d4ecc0607ffb6ecbd26545
zachlegros <legroszach3921@gmail.com>
2024-01-02 17:27:33 -0500

    Added nextui library

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,23 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import { nextui } from "@nextui-org/react";
+
+export default {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
         btn: {
-          background: 'hsl(var(--btn-background))',
-          'background-hover': 'hsl(var(--btn-background-hover))',
+          background: "hsl(var(--btn-background))",
+          "background-hover": "hsl(var(--btn-background-hover))",
         },
       },
     },
   },
-  plugins: [],
-}
+  darkMode: "class",
+  plugins: [nextui()],
+};
