10f37c5838ad7c8b3b1f141892b889d1a1383fb8
zachlegros <legroszach3921@gmail.com>
2024-01-03 18:33:17 -0500

    Refactor Materials component to include filtering and sorting functionality

--- app/custom-order/materials.tsx
+++ app/custom-order/materials.tsx
@@ -2,15 +2,41 @@
 
 import ItemsGrid from "@/components/ItemsGrid";
 import MaterialCard from "@/components/MaterialCard";
-import { Material } from "@prisma/client";
+import { useCustomOrderStore } from "./store";
+import { useMemo } from "react";
 
-export default function Materials(props: { materials: Material[] }) {
-  const { materials } = props;
+export default function Materials(props: { onSelect: (materialId: string) => void }) {
+  const { onSelect } = props;
+  const { materials, typeFilter, originFilter, priceFilter } = useCustomOrderStore();
+
+  const filteredMaterials = useMemo(() => {
+    const mats = materials.filter((material) => {
+      if (typeFilter.enabled && typeFilter.value !== "all" && material.type !== typeFilter.value)
+        return false;
+      if (
+        originFilter.enabled &&
+        originFilter.value !== "all" &&
+        material.origin !== originFilter.value
+      )
+        return false;
+      return true;
+    });
+
+    if (priceFilter.enabled) {
+      mats.sort((a, b) => {
+        if (priceFilter.value === "asc") return a.price - b.price;
+        if (priceFilter.value === "desc") return b.price - a.price;
+        return 0;
+      });
+    }
+
+    return mats;
+  }, [materials, typeFilter, priceFilter, originFilter]);
 
   return (
-    <ItemsGrid className="animate-in">
-      {materials.map((material) => (
-        <MaterialCard material={material} key={material.id} />
+    <ItemsGrid className="animate-in w-full">
+      {filteredMaterials.map((material) => (
+        <MaterialCard material={material} key={material.id} onClick={() => onSelect(material.id)} />
       ))}
     </ItemsGrid>
   );
