8b49b08c58f1e7c90395326cfb0ffc49af22a307
zachlegros <legroszach3921@gmail.com>
2024-01-03 18:59:12 -0500

    Add HardwareCard component

---
+++ components/HardwareCard.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Hardware } from "@prisma/client";
+import { Card, CardBody, Chip } from "@nextui-org/react";
+import Image from "next/image";
+import { cad } from "@/utils/currencyFormatter";
+
+export default function HardwareCard(props: { hardware: Hardware; onClick: () => void }) {
+  const { hardware, onClick } = props;
+  return (
+    <Card
+      shadow="sm"
+      className="bg-background/60 dark:bg-default-100/50 "
+      onPress={onClick}
+      isPressable
+      isHoverable
+    >
+      <CardBody className="flex flex-row gap-4">
+        <Image
+          width={125}
+          height={125}
+          src={hardware.image}
+          alt={hardware.name}
+          className="rounded-md object-contain self-start"
+          loading="lazy"
+        />
+        <div className="flex flex-1 flex-col gap-2">
+          <div className="flex justify-between gap-4 text-lg">
+            <b>{hardware.name}</b>
+            <p>{cad(hardware.price)}</p>
+          </div>
+          <div className="flex gap-2 flex-wrap">
+            <Chip radius="sm" size="sm" variant="flat" color="warning">
+              {hardware.color}
+            </Chip>
+          </div>
+        </div>
+      </CardBody>
+    </Card>
+  );
+}
