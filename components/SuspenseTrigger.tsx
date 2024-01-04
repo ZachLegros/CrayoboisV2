98769374603a820b61e4a2c8408617f4db620e9a
zachlegros <legroszach3921@gmail.com>
2024-01-04 18:37:01 -0500

    Add SuspenseTrigger component to trigger loading UI

---
+++ components/SuspenseTrigger.tsx
@@ -0,0 +1,6 @@
+// insert this component in the client component to trigger the loading UI
+export default function SuspenseTrigger() {
+  // this triggers Suspense
+  throw new Promise(() => {});
+  return <></>;
+}
