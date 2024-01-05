3c18050d76ae6e16501e1d64ec1760c8be5533ac
zachlegros <legroszach3921@gmail.com>
2024-01-04 23:22:19 -0500

    Add ImageWithLoading component

---
+++ components/ImageWithLoading.tsx
@@ -0,0 +1,20 @@
+import { Skeleton } from "@nextui-org/react";
+import Image, { type ImageProps } from "next/image";
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
+
+export type ImageWithLoadingProps = Omit<ImageProps, "onLoadingComplete" | "loading"> & {
+  className?: string;
+};
+
+export default function ImageWithLoading(props: ImageWithLoadingProps) {
+  const { className, src, ...rest } = props;
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  return (
+    <div className={twMerge("relative overflow-hidden", className)}>
+      <Image src={src} {...rest} onLoadingComplete={() => setIsLoaded(true)} loading="lazy" />
+      {!isLoaded && <Skeleton className="absolute top-0 left-0 w-full h-full" />}
+    </div>
+  );
+}
