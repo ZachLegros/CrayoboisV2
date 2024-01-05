52fc3736940f3138ac0fe47cce21133c6dcf2c01
zachlegros <legroszach3921@gmail.com>
2024-01-05 17:02:21 -0500

    Add products state and setter function to ProductsStore

--- app/products/store.ts
+++ app/products/store.ts
@@ -1,15 +1,22 @@
 import { create } from "zustand";
 import { Filter, PriceFilterValue, createFilter } from "../custom-order/store";
+import { Product } from "@prisma/client";
 
 const getInitialPriceFilter = (): PriceFilterValue => "desc";
 
 export type ProductsStore = {
+  products: readonly Product[];
+  setProducts: (products: Product[]) => void;
   priceFilter: Filter;
   clearFilters: () => void;
   reset: () => void;
 };
 
 export const useProductsStore = create<ProductsStore>((set, state) => ({
+  products: [],
+  setProducts: (products: Product[]) => {
+    set({ products });
+  },
   priceFilter: createFilter(set, "priceFilter", true, getInitialPriceFilter),
   clearFilters: () => {
     state().priceFilter.clear();
