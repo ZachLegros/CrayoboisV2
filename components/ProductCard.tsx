2023c21527b8f97aec8850bb8dcb12ef6e403984
zachlegros <legroszach3921@gmail.com>
2024-01-05 17:01:59 -0500

    Refactor ProductCard component and add expand button

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -5,33 +5,45 @@ import { cad } from "@/utils/currencyFormatter";
 import ImageWithLoading from "./ImageWithLoading";
 import { NonNullabbleProduct } from "@/utils/customProductFactory";
 import { useCartStore } from "@/app/cart/store";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { FaExpandAlt } from "react-icons/fa";
+import { Tooltip } from "@nextui-org/react";
+import { toast } from "sonner";
 
 export default function ProductCard(props: { product: NonNullabbleProduct; onClick: () => void }) {
   const { product } = props;
-  const { cart, addToCart, isProductInCart } = useCartStore();
-  const [isInCart, setIsInCart] = useState(false);
+  const { addToCart, isProductInCart } = useCartStore();
+  const [isInCart, setIsInCart] = useState(isProductInCart(product));
 
   const handleAddToCart = () => {
     addToCart(product);
     setIsInCart(true);
+    toast.success(`"${product.name}" a été ajouté à votre panier.`);
   };
-
-  useEffect(() => {
-    setIsInCart(isProductInCart(product));
-  }, [cart]);
 
   return (
     <Card shadow="sm" className="bg-background/60 dark:bg-default-100/50 " isFooterBlurred>
       <CardBody className="flex flex-col gap-4">
-        <ImageWithLoading
-          width={225}
-          height={225}
-          src={product.image}
-          alt={product.name}
-          className="rounded-md !w-full"
-          quality={100}
-        />
+        <div className="relative">
+          <ImageWithLoading
+            width={225}
+            height={225}
+            src={product.image}
+            alt={product.name}
+            className="rounded-md"
+            quality={100}
+          />
+          <Tooltip showArrow content="Agrandir">
+            <Button
+              isIconOnly
+              variant="flat"
+              size="sm"
+              className="absolute bottom-0 right-0 m-1 hover:bg-default/90"
+            >
+              <FaExpandAlt className="text-lg" />
+            </Button>
+          </Tooltip>
+        </div>
         <div className="flex flex-col gap-2 flex-1">
           <div className="flex flex-col gap-2 text-lg">
             <b>{product.name}</b>
