d3269f561af112318671cd16eaa58b2085c48bd6
zachlegros <legroszach3921@gmail.com>
2024-01-05 18:16:51 -0500

    Refactor product filtering logic in ProductsGrid component

--- app/products/products.tsx
+++ app/products/products.tsx
@@ -5,20 +5,33 @@ import ProductCard from "@/components/ProductCard";
 import { NonNullabbleProduct } from "@/utils/customProductFactory";
 import { Button } from "@nextui-org/react";
 import { Product } from "@prisma/client";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { useProductsStore } from "./store";
 import { useRouter } from "next/navigation";
 
 export default function ProductsGrid(props: { products: Product[] }) {
   const { products } = props;
   const router = useRouter();
-  const { setProducts } = useProductsStore();
+  const { priceFilter, setProducts } = useProductsStore();
 
   useEffect(() => {
     setProducts(products);
   }, []);
 
-  if (products.length === 0)
+  const filteredProducts = useMemo(() => {
+    if (products.length === 0) return [];
+    const prods = [...products];
+    if (priceFilter.enabled) {
+      prods.sort((a, b) => {
+        if (priceFilter.value === "asc") return a.price - b.price;
+        if (priceFilter.value === "desc") return b.price - a.price;
+        return 0;
+      });
+    }
+    return prods;
+  }, [products, priceFilter]);
+
+  if (filteredProducts.length === 0)
     return (
       <div className="flex flex-col items-center justify-start w-full h-full mt-8">
         <p className="text-3xl font-bold">Tous nos produits préfabriqués sont vendus!</p>
@@ -35,7 +48,7 @@ export default function ProductsGrid(props: { products: Product[] }) {
 
   return (
     <ItemsGrid className="md:grid-cols-2 lg:grid-cols-3">
-      {products.map((product) => (
+      {filteredProducts.map((product) => (
         <ProductCard key={product.id} product={product as NonNullabbleProduct} onClick={() => {}} />
       ))}
     </ItemsGrid>
