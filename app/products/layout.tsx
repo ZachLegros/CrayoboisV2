b5122728ea0f2bd096fdfdc84b7e0f471c7c8f3f
zachlegros <legroszach3921@gmail.com>
2024-01-05 15:35:08 -0500

    Add layout component for products page

---
+++ app/products/layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+import SidePanel from "./side-panel";
+
+export default function Layout(props: { children: ReactNode }) {
+  const { children } = props;
+
+  return (
+    <div className="flex w-full gap-4">
+      <SidePanel />
+      <div className="flex-1">{children}</div>
+    </div>
+  );
+}
