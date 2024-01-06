9a19c271a761657129fcd5adbdf411bb16630f6c
zachlegros <legroszach3921@gmail.com>
2024-01-05 22:24:46 -0500

    Add AddedToCart component

---
+++ app/custom-order/added-to-cart.tsx
@@ -0,0 +1,33 @@
+import { Button, Link } from "@nextui-org/react";
+import NextLink from "next/link";
+import { useCustomOrderStore } from "./store";
+import { useEffect } from "react";
+
+export default function AddedToCart() {
+  const { reset } = useCustomOrderStore();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center justify-start w-full h-full mt-8">
+      <p className="text-3xl font-bold">
+        Votre produit a été ajouté au{" "}
+        <Link
+          as={NextLink}
+          href="/cart"
+          className="text-3xl font-bold cursor-pointer"
+          color="primary"
+          underline="hover"
+          onClick={() => reset()}
+        >
+          panier
+        </Link>
+      </p>
+      <Button color="primary" className="mt-8 font-semibold" size="lg" onClick={() => reset()}>
+        Créer un autre produit
+      </Button>
+    </div>
+  );
+}
