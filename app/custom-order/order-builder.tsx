fd96b9a5d0bf20aca50f76e30acde2623f19637b
zachlegros <legroszach3921@gmail.com>
2024-01-05 22:25:15 -0500

    Refactor order builder component and add toast notification

--- app/custom-order/order-builder.tsx
+++ app/custom-order/order-builder.tsx
@@ -3,16 +3,16 @@
 import { useEffect } from "react";
 import { Breadcrumbs } from "@/components/Breadcrumbs";
 import { Hardware, Material } from "@prisma/client";
-import { useRouter } from "next/navigation";
 import Materials from "./materials";
 import { useCustomOrderStore } from "./store";
 import Hardwares from "./hardwares";
 import { useCartStore } from "../cart/store";
 import { customProductFactory } from "@/utils/customProductFactory";
+import AddedToCart from "./added-to-cart";
+import { toast } from "sonner";
 
 export default function OrderBuilder(props: { materials: Material[]; hardwares: Hardware[] }) {
   const { materials, hardwares } = props;
-  const router = useRouter();
   const { addToCart } = useCartStore();
   const {
     currentStep,
@@ -22,14 +22,13 @@ export default function OrderBuilder(props: { materials: Material[]; hardwares: 
     selectMaterial,
     selectHardware,
     selectedMaterial,
-    reset,
   } = useCustomOrderStore();
 
   const handleAddToCart = (material: Material, hardware: Hardware) => {
     const customProduct = customProductFactory(material, hardware);
     addToCart(customProduct);
-    reset();
-    router.push("/cart");
+    setCurrentStep(currentStep + 1);
+    toast.success("Votre produit a été ajouté au panier");
   };
 
   useEffect(() => {
@@ -39,20 +38,22 @@ export default function OrderBuilder(props: { materials: Material[]; hardwares: 
 
   return (
     <div className="flex flex-col w-full gap-4">
-      <Breadcrumbs
-        steps={["Choix du bois", "Choix du matériel", "Ajouter au panier"]}
-        currentStep={currentStep}
-        onAction={(stepIndex) => {
-          if (stepIndex === 0) {
-            selectMaterial(null);
-            selectHardware(null);
-          }
-          if (stepIndex === 1) {
-            selectHardware(null);
-          }
-          setCurrentStep(stepIndex);
-        }}
-      />
+      {currentStep < 2 && (
+        <Breadcrumbs
+          steps={["Choix du bois", "Choix du matériel", "Ajouter au panier"]}
+          currentStep={currentStep}
+          onAction={(stepIndex) => {
+            if (stepIndex === 0) {
+              selectMaterial(null);
+              selectHardware(null);
+            }
+            if (stepIndex === 1) {
+              selectHardware(null);
+            }
+            setCurrentStep(stepIndex);
+          }}
+        />
+      )}
       {currentStep === 0 && (
         <Materials
           onSelect={(material) => {
@@ -69,6 +70,7 @@ export default function OrderBuilder(props: { materials: Material[]; hardwares: 
           }}
         />
       )}
+      {currentStep === 2 && <AddedToCart />}
     </div>
   );
 }
