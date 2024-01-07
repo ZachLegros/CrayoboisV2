dab1c44251634158d7dbe54ffc6695ebc1482ace
zachlegros <legroszach3921@gmail.com>
2024-01-07 12:30:09 -0500

    Refactor skeleton loading styles

--- app/cart/loading.tsx
+++ app/cart/loading.tsx
@@ -5,18 +5,18 @@ export default function Loading() {
     <>
       <div className="flex flex-col flex-1">
         <div className="flex flex-col gap-4 mb-4 w-full h-full">
-          <Skeleton className="flex flex-col w-full h-[150px]" />
+          <Skeleton className="flex flex-col w-full h-[150px] rounded-md" />
           <Divider />
         </div>
         <div className="flex flex-col gap-4 mb-4 w-full h-full">
-          <Skeleton className="flex flex-col w-full h-[150px]" />
+          <Skeleton className="flex flex-col w-full h-[150px] rounded-md" />
           <Divider />
         </div>
         <div className="flex flex-col gap-4 mb-4 w-full h-full">
-          <Skeleton className="flex flex-col w-full h-[150px]" />
+          <Skeleton className="flex flex-col w-full h-[150px] rounded-md" />
         </div>
       </div>
-      <Skeleton className="w-72 h-[440px] overflow-hidden" />
+      <Skeleton className="w-72 h-[440px] overflow-hidden rounded-md" />
     </>
   );
 }
