0444a3c860df3bad2c6ae6c3e2521f079c99635e
zachlegros <legroszach3921@gmail.com>
2024-01-07 00:02:19 -0500

    Refactor component card layout

--- components/HardwareCard.tsx
+++ components/HardwareCard.tsx
@@ -15,24 +15,28 @@ export default function HardwareCard(props: { hardware: Hardware; onClick: () =>
       isPressable
       isHoverable
     >
-      <CardBody className="flex flex-row gap-4">
-        <ImageWithLoading
-          width={150}
-          height={150}
-          src={hardware.image}
-          alt={hardware.name}
-          className="rounded-md max-w-[150px] max-h-[150px]"
-          quality={80}
-        />
-        <div className="flex flex-auto flex-col gap-2">
-          <div className="flex justify-between gap-4 text-lg">
-            <b>{hardware.name}</b>
-            <p>{cad(hardware.price)}</p>
+      <CardBody>
+        <div className="grid flex-row grid-cols-component-card-sm md:grid-cols-component-card-md lg:grid-cols-component-card-lg gap-4">
+          <div className="w-[100px] md:w-[125px] lg:w-[150px]">
+            <ImageWithLoading
+              width={150}
+              height={150}
+              src={hardware.image}
+              alt={hardware.name}
+              className="rounded-md max-w-[150px] max-h-[150px]"
+              quality={80}
+            />
           </div>
-          <div className="flex gap-2 flex-wrap">
-            <Chip radius="sm" size="sm" variant="flat" color="warning">
-              {hardware.color}
-            </Chip>
+          <div className="flex flex-auto flex-col gap-2">
+            <div className="flex justify-between gap-4 text-lg">
+              <b>{hardware.name}</b>
+              <p>{cad(hardware.price)}</p>
+            </div>
+            <div className="flex gap-2 flex-wrap">
+              <Chip radius="sm" size="sm" variant="flat" color="warning">
+                {hardware.color}
+              </Chip>
+            </div>
           </div>
         </div>
       </CardBody>
--- components/MaterialCard.tsx
+++ components/MaterialCard.tsx
@@ -16,29 +16,33 @@ export default function MaterialCard(props: { material: Material; onClick: () =>
       isPressable
       isHoverable
     >
-      <CardBody className="flex flex-row gap-4">
-        <ImageWithLoading
-          width={150}
-          height={150}
-          src={material.image}
-          alt={material.name}
-          className="rounded-md max-w-[150px] max-h-[150px]"
-          quality={80}
-        />
-        <div className="flex flex-auto flex-col gap-2">
-          <div className="flex justify-between gap-4 text-lg">
-            <b>{material.name}</b>
-            <p>{cad(material.price)}</p>
+      <CardBody>
+        <div className="grid flex-row grid-cols-component-card-sm md:grid-cols-component-card-md lg:grid-cols-component-card-lg gap-4">
+          <div className="w-[100px] md:w-[125px] lg:w-[150px]">
+            <ImageWithLoading
+              width={150}
+              height={150}
+              src={material.image}
+              alt={material.name}
+              className="rounded-md max-w-[150px] max-h-[150px]"
+              quality={80}
+            />
           </div>
-          <div className="flex gap-2 flex-wrap">
-            <div className="flex text-sm items-center gap-1">
-              <FaEarthAmericas className="flex" />
-              <p>{material.origin}</p>
+          <div className="flex flex-col gap-2">
+            <div className="flex justify-between gap-4 text-lg">
+              <b>{material.name}</b>
+              <p>{cad(material.price)}</p>
             </div>
-            {/* <p>•</p> */}
-            <Chip radius="sm" size="sm" variant="flat" color="warning">
-              {material.type}
-            </Chip>
+            <div className="flex gap-2 flex-wrap">
+              <div className="flex text-sm items-center gap-1">
+                <FaEarthAmericas className="flex" />
+                <p>{material.origin}</p>
+              </div>
+              {/* <p>•</p> */}
+              <Chip radius="sm" size="sm" variant="flat" color="warning">
+                {material.type}
+              </Chip>
+            </div>
           </div>
         </div>
       </CardBody>
