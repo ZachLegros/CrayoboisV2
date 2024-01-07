3e1a7a56aafa6f245842e028b69da24c5b3a5c02
zachlegros <legroszach3921@gmail.com>
2024-01-07 18:40:08 -0500

    Refactor CartItem component to improve readability and maintainability

--- app/cart/cart-item.tsx
+++ app/cart/cart-item.tsx
@@ -1,20 +1,72 @@
 import { twMerge } from "tailwind-merge";
 import { cad } from "@/utils/currencyFormatter";
-import { Chip, Button, Select, SelectItem } from "@nextui-org/react";
-import { CartItemType, getProductMaxQuantity, useCartStore } from "./store";
-import { ChangeEvent } from "react";
-import {
-  NonNullabbleProduct,
-  NonNullabbleProductWithComponents,
-} from "@/utils/customProductFactory";
+import { Chip, Button, Select, SelectItem, Divider } from "@nextui-org/react";
+import { CartItemType, CartProductType, useCartStore } from "./store";
+import { ChangeEvent, useState } from "react";
 import ImageWithLoading from "@/components/ImageWithLoading";
 import Image from "next/image";
 import ImageListWithLoading from "@/components/ImageListWithLoading";
+import { DbProduct, isCustomProductWithComponents, isProduct } from "@/utils/productUtils";
 
-export default function CartItem(props: { item: CartItemType }) {
-  const { item } = props;
-  const { product } = item;
-  const { removeFromCart, setItemQuantity } = useCartStore();
+const getImageComponents = (product: DbProduct) => {
+  if (isCustomProductWithComponents(product)) {
+    const { material, hardware } = product;
+    return (
+      <ImageListWithLoading itemsNo={2}>
+        {(onLoad) => (
+          <>
+            <Image
+              width={75}
+              height={75}
+              src={material.image}
+              alt={material.name}
+              quality={70}
+              loading="eager"
+              onLoad={onLoad}
+            />
+            <Image
+              width={75}
+              height={75}
+              src={hardware.image}
+              alt={hardware.name}
+              quality={70}
+              loading="eager"
+              onLoad={onLoad}
+            />
+          </>
+        )}
+      </ImageListWithLoading>
+    );
+  }
+  if (isProduct(product)) {
+    return (
+      <ImageWithLoading
+        width={150}
+        height={150}
+        src={product.image}
+        alt={product.name}
+        quality={80}
+      />
+    );
+  }
+  return null;
+};
+
+export default function CartItem(props: {
+  item: CartItemType<CartProductType>;
+  hasDivider?: boolean;
+}) {
+  const { item, hasDivider } = props;
+  const { product: cartItem } = item;
+  const { removeFromCart, setItemQuantity, cartItemData } = useCartStore();
+
+  const product = cartItemData[cartItem.id];
+  const [quantityRange] = useState([...Array(100 + 1).keys()].slice(1, 100 + 1));
+
+  const handleQuantityChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const quantity = parseInt(e.target.value);
+    setItemQuantity(cartItem, quantity);
+  };
 
   const Section = (props: { title: string; className?: string; children: React.ReactNode }) => {
     const { title, className, children } = props;
@@ -26,97 +78,53 @@ export default function CartItem(props: { item: CartItemType }) {
     );
   };
 
-  const getQuantityRange = (quantity: number) =>
-    [...Array(quantity + 1).keys()].slice(1, quantity + 1);
-
-  const handleQuantityChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    const quantity = parseInt(e.target.value);
-    setItemQuantity(product, quantity);
-  };
-
-  const getImageComponents = () => {
-    if (product.is_custom) {
-      const productWithComponents = product as NonNullabbleProductWithComponents;
-      const { material, hardware } = productWithComponents;
-
-      return (
-        <ImageListWithLoading itemsNo={2}>
-          {(onLoad) => (
-            <>
-              <Image
-                width={75}
-                height={75}
-                src={material.image}
-                alt={material.name}
-                quality={70}
-                loading="eager"
-                onLoad={onLoad}
-              />
-              <Image
-                width={75}
-                height={75}
-                src={hardware.image}
-                alt={hardware.name}
-                quality={70}
-                loading="eager"
-                onLoad={onLoad}
-              />
-            </>
-          )}
-        </ImageListWithLoading>
-      );
-    }
-    return (
-      <ImageWithLoading
-        width={150}
-        height={150}
-        src={(product as NonNullabbleProduct).image}
-        alt={product.name}
-        quality={80}
-      />
-    );
-  };
+  if (product === undefined) return null;
 
   return (
-    <div className="grid grid-cols-cart-item gap-4 h-[150px] w-full">
-      <div className="flex rounded-md overflow-hidden w-[150px] h-max">{getImageComponents()}</div>
-      <Section title={product.name}>
-        <span>{cad(product.price)}</span>
-      </Section>
-      <Section title="Quantité">
-        {product.is_custom || product.quantity > 1 ? (
-          <Select
-            size="sm"
-            defaultSelectedKeys={[`${item.quantity}`]}
-            onChange={handleQuantityChange}
-            aria-label="Quantité"
-          >
-            {getQuantityRange(getProductMaxQuantity(product)).map((quantity) => (
-              <SelectItem key={`${quantity}`} value={`${quantity}`}>
-                {`${quantity}`}
-              </SelectItem>
-            ))}
-          </Select>
-        ) : (
-          <Chip color="warning" variant="dot">
-            1 en stock
-          </Chip>
-        )}
-      </Section>
-      <Section title="Sous-total" className="items-end">
-        <div className="flex flex-col justify-between h-full">
-          <span>{cad(product.price * item.quantity)}</span>
-          <Button
-            size="sm"
-            color="danger"
-            variant="flat"
-            onClick={() => removeFromCart(product)}
-            className="w-max ml-auto"
-          >
-            Retirer
-          </Button>
+    <>
+      <div className="grid grid-cols-cart-item gap-4 h-[150px] w-full">
+        <div className="flex rounded-md overflow-hidden w-[150px] h-max">
+          {getImageComponents(product)}
         </div>
-      </Section>
-    </div>
+        <Section title={product.name}>
+          <span>{cad(product.price)}</span>
+        </Section>
+        <Section title="Quantité">
+          {isCustomProductWithComponents(product) || product.quantity > 1 ? (
+            <Select
+              size="sm"
+              defaultSelectedKeys={[`${item.quantity}`]}
+              onChange={handleQuantityChange}
+              aria-label="Quantité"
+            >
+              {quantityRange.map((quantity) => (
+                <SelectItem key={`${quantity}`} value={`${quantity}`}>
+                  {`${quantity}`}
+                </SelectItem>
+              ))}
+            </Select>
+          ) : (
+            <Chip color="warning" variant="dot">
+              1 en stock
+            </Chip>
+          )}
+        </Section>
+        <Section title="Sous-total" className="items-end">
+          <div className="flex flex-col justify-between h-full">
+            <span>{cad(product.price * item.quantity)}</span>
+            <Button
+              size="sm"
+              color="danger"
+              variant="flat"
+              onClick={() => removeFromCart(product)}
+              className="w-max ml-auto"
+            >
+              Retirer
+            </Button>
+          </div>
+        </Section>
+      </div>
+      {hasDivider && <Divider />}
+    </>
   );
 }
