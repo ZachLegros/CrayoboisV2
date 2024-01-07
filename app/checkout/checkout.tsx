440503a552c20cc3effc437ec9547a4bfa050188
zachlegros <legroszach3921@gmail.com>
2024-01-07 18:57:21 -0500

    Add cart data to checkout session request

--- app/checkout/checkout.tsx
+++ app/checkout/checkout.tsx
@@ -4,15 +4,18 @@ import { useEffect, useState } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { EmbeddedCheckoutProvider, EmbeddedCheckout } from "@stripe/react-stripe-js";
 import { Card, CardBody, Spinner } from "@nextui-org/react";
+import { useCartStore } from "../cart/store";
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
 export default function Checkout() {
+  const { cart } = useCartStore();
   const [clientSecret, setClientSecret] = useState("");
 
   useEffect(() => {
     fetch("/api/checkout_sessions", {
       method: "POST",
+      body: JSON.stringify(cart),
     })
       .then((res) => res.json())
       .then((data) => setClientSecret(data.clientSecret));
