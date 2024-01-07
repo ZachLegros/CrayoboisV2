da2c00332367d808b251632fd1bc61ec111087bb
zachlegros <legroszach3921@gmail.com>
2024-01-07 16:36:57 -0500

    Refactor cart store and add new types

--- app/cart/store.ts
+++ app/cart/store.ts
@@ -1,29 +1,47 @@
-import { Product, Shipping } from "@prisma/client";
+import { Shipping } from "@prisma/client";
 import { create } from "zustand";
-import { NonNullabbleProduct, ProductWithComponents } from "@/utils/customProductFactory";
-import { fetchProducts } from "./actions";
+import { syncCart } from "./actions";
 
-export type CartItemType = {
-  product: Product | ProductWithComponents;
+export type CartProduct = { id: string };
+export type CartCustomProduct = CartProduct & { material_id: string; hardware_id: string };
+export type CartProductType = CartProduct | CartCustomProduct;
+
+export const isCartCustomProduct = (
+  cartProduct: CartProductType
+): cartProduct is CartCustomProduct => {
+  return (
+    (cartProduct as CartCustomProduct).material_id !== undefined &&
+    (cartProduct as CartCustomProduct).hardware_id !== undefined
+  );
+};
+
+export const getCartProductMaterialId = (product: CartProductType) => {
+  return isCartCustomProduct(product) ? product.material_id : undefined;
+};
+
+export const getCartProductHardwareId = (product: CartProductType) => {
+  return isCartCustomProduct(product) ? product.hardware_id : undefined;
+};
+
+export type CartItemType<T> = {
+  product: T;
   quantity: number;
 };
 
-export const getProductMaxQuantity = (product: Product | ProductWithComponents) => {
-  if (product.is_custom) {
-    return Math.min(
-      (product as ProductWithComponents).material.quantity,
-      (product as ProductWithComponents).hardware.quantity
-    );
-  }
-  return product.quantity;
+export const isCartItemType = (
+  item: CartItemType<CartProductType>
+): item is CartItemType<CartProductType> => {
+  return item.product !== undefined && !isNaN(item.quantity);
 };
 
-const storeCartInLocalStorage = (cart: CartItemType[]) => {
+const storeCartInLocalStorage = (cart: CartItemType<CartProductType>[]) => {
   if (typeof localStorage === "undefined") return;
   localStorage.setItem("cart", JSON.stringify(cart));
 };
 
-const initializeCartFromLocalStorage = (callback?: (cart: CartItemType[]) => void) => {
+const initializeCartFromLocalStorage = (
+  callback?: (cart: CartItemType<CartProductType>[]) => void
+) => {
   if (typeof localStorage === "undefined") return [];
   const cartString = localStorage.getItem("cart");
   const cart = cartString ? JSON.parse(cartString) : [];
@@ -31,72 +49,32 @@ const initializeCartFromLocalStorage = (callback?: (cart: CartItemType[]) => voi
   return cart;
 };
 
-async function syncProducts(cart: CartItemType[], setCart: (cart: CartItemType[]) => void) {
-  const normalProductTemplates = cart
-    .filter((item) => !item.product.is_custom)
-    .map((item) => item.product);
-  const customProductTemplates = cart
-    .filter((item) => item.product.is_custom)
-    .map((item) => item.product as NonNullabbleProduct);
-  const products = await fetchProducts(normalProductTemplates, customProductTemplates);
-  const inSyncCartItemTypes: CartItemType[] = cart
-    // Filter out items that are no longer available
-    .filter((item) => {
-      if (item.product.is_custom) {
-        return products.some(
-          (product) =>
-            item.product.material_id === product.material_id &&
-            item.product.hardware_id === product.hardware_id
-        );
-      } else {
-        return products.some((product) => product.id === item.product.id);
-      }
-    })
-    .map((item) => {
-      const product = item.product.is_custom
-        ? products.find(
-            (product) =>
-              item.product.material_id === product.material_id &&
-              item.product.hardware_id === product.hardware_id
-          )
-        : products.find((product) => product.id === item.product.id);
-      if (product) {
-        const closestMaxQuantity = Math.min(item.quantity, getProductMaxQuantity(product));
-        return {
-          product,
-          quantity: closestMaxQuantity > 0 ? closestMaxQuantity : 1,
-        };
-      }
-    })
-    .filter((item): item is CartItemType => !!item);
-  storeCartInLocalStorage(inSyncCartItemTypes);
-  setCart(inSyncCartItemTypes);
-}
-
 const isProductInCart = (
-  product: Product | ProductWithComponents,
-  cart: readonly CartItemType[]
+  product: CartProductType,
+  cart: readonly CartItemType<CartProductType>[]
 ) => {
   return cart.some((item) => item.product.id === product.id);
 };
 
 export type CartStore = {
-  cart: readonly CartItemType[];
-  addToCart: (product: Product | ProductWithComponents) => void;
-  removeFromCart: (product: Product | ProductWithComponents) => void;
+  cart: readonly CartItemType<CartProductType>[];
+  addToCart: (product: CartProductType) => void;
+  removeFromCart: (product: CartProductType) => void;
   clearCart: () => void;
-  setItemQuantity: (product: Product | ProductWithComponents, quantity: number) => void;
+  setItemQuantity: (product: CartProductType, quantity: number) => void;
   shippingMethods: readonly Shipping[];
   setShippingMethods: (shippingMethods: Shipping[]) => void;
   shippingMethod: Shipping | null;
-  setShippingMethod: (id: number) => void;
-  isProductInCart: (product: Product | ProductWithComponents) => boolean;
+  setShippingMethod: (id: string) => void;
+  isProductInCart: (product: CartProductType) => boolean;
 };
 
 export const useCartStore = create<CartStore>((set, state) => ({
-  cart: initializeCartFromLocalStorage((cart) =>
-    syncProducts(cart, (cart) => set(() => ({ cart })))
-  ),
+  cart: initializeCartFromLocalStorage(async (cart) => {
+    const syncedCart = await syncCart(cart);
+    storeCartInLocalStorage(syncedCart);
+    set(() => ({ cart: syncedCart }));
+  }),
   addToCart: (product) => {
     const alreadyInCart = state().cart.find((item) => item.product.id === product.id);
     if (alreadyInCart) return;
@@ -124,6 +102,6 @@ export const useCartStore = create<CartStore>((set, state) => ({
   shippingMethods: [],
   setShippingMethods: (shippingMethods) => set(() => ({ shippingMethods })),
   shippingMethod: null,
-  setShippingMethod: (id: number) =>
+  setShippingMethod: (id: string) =>
     set(() => ({ shippingMethod: state().shippingMethods.find((item) => item.id === id) })),
 }));
