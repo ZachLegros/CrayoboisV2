7b71e2aefbad4cfd06005a452884210c716b94e2
zachlegros <legroszach3921@gmail.com>
2024-01-06 23:50:09 -0500

    Update ESLint and Tailwind configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 import { nextui } from "@nextui-org/react";
 
-export default {
+const config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -18,9 +18,41 @@ export default {
       },
       gridTemplateColumns: {
         "cart-item": "150px auto 125px 150px",
+        "component-card-lg": "150px auto",
+        "component-card-md": "125px auto",
+        "component-card-sm": "100px auto",
       },
     },
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [
+    nextui({
+      prefix: "nextui", // prefix for themes variables
+      addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
+      defaultTheme: "light", // default theme from the themes object
+      defaultExtendTheme: "light", // default theme to extend on custom themes
+      layout: {}, // common layout tokens (applied to all themes)
+      themes: {
+        light: {
+          layout: {}, // light theme layout tokens
+          colors: {
+            primary: "#cc5500",
+            secondary: "#2E86AB",
+            background: "#ededed",
+          }, // light theme colors
+        },
+        dark: {
+          layout: {}, // dark theme layout tokens
+          colors: {
+            primary: "#cc5500",
+            secondary: "#2E86AB",
+            background: "#0b0e10",
+          }, // dark theme colors
+        },
+        // ... custom themes
+      },
+    }),
+  ],
 };
+
+export default config;
