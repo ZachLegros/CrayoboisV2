96c27d48d1b96e5eb86f7075035ae31e0ea098e5
zachlegros <legroszach3921@gmail.com>
2024-01-07 22:24:45 -0500

    Add support for retrieving checkout session by session ID

--- app/checkout/checkout.tsx
+++ app/checkout/checkout.tsx
@@ -5,46 +5,64 @@ import { loadStripe } from "@stripe/stripe-js";
 import { EmbeddedCheckoutProvider, EmbeddedCheckout } from "@stripe/react-stripe-js";
 import { Card, CardBody, Spinner } from "@nextui-org/react";
 import { useCartStore } from "../cart/store";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { toast } from "sonner";
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
 export default function Checkout() {
   const router = useRouter();
+  const searchParams = useSearchParams();
   const { cart, syncCart } = useCartStore();
   const [clientSecret, setClientSecret] = useState("");
+  const sessionId = searchParams.get("session_id");
 
   useEffect(() => {
-    fetch("/api/checkout_sessions", {
-      method: "POST",
-      body: JSON.stringify(cart),
-    })
-      .then(async (res) => {
-        const data = await res.json();
-        if (res.status === 200) {
-          setClientSecret(data.clientSecret);
-        } else {
-          if (data.error === "cart_is_empty") {
-            toast.error("Votre panier est vide.");
-            router.push("/cart");
-          } else if (data.error === "cart_out_of_sync") {
-            syncCart();
-            toast.error("Un ou plusieurs produits de votre panier ne sont plus disponibles.");
-            router.push("/cart");
-          } else {
-            toast.error("Une erreur inattendu est survenue. Veuillez réessayer ou nous contacter.");
-            console.log(data.error);
-          }
-        }
+    if (!cart.length && !sessionId && !clientSecret) {
+      fetch("/api/checkout_sessions", {
+        method: "POST",
+        body: JSON.stringify(cart),
+        credentials: "same-origin",
       })
-      .catch((err) => console.log(err));
+        .then(async (res) => {
+          const data = await res.json();
+          if (res.status === 200) {
+            setClientSecret(data.clientSecret);
+          } else {
+            if (data.error === "cart_is_empty") {
+              toast.error("Votre panier est vide.");
+              router.push("/cart");
+            } else if (data.error === "cart_out_of_sync") {
+              syncCart();
+              toast.error("Un ou plusieurs produits de votre panier ne sont plus disponibles.");
+              router.push("/cart");
+            } else {
+              toast.error(
+                "Une erreur inattendu est survenue. Veuillez réessayer ou nous contacter."
+              );
+              console.log(data.error);
+            }
+          }
+        })
+        .catch((err) => console.log(err));
+    } else if (sessionId) {
+      fetch(`/api/checkout_sessions?session_id=${sessionId}`, {
+        method: "GET",
+      }).then(async (res) => {
+        const data = await res.json();
+        console.log(data);
+      });
+    } else if (cart.length === 0) {
+      router.push("/cart");
+    }
   }, []);
 
   return (
     <Card id="checkout" className="animate-in max-w-screen-sm mx-auto relative">
       <CardBody className={clientSecret ? "h-auto" : "flex justify-center items-center h-[650px]"}>
-        {clientSecret ? (
+        {sessionId ? (
+          <></>
+        ) : clientSecret ? (
           <EmbeddedCheckoutProvider stripe={stripePromise} options={{ clientSecret }}>
             <EmbeddedCheckout className="rounded-lg overflow-hidden" />
           </EmbeddedCheckoutProvider>
