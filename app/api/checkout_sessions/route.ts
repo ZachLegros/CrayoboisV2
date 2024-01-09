f0dbacee8a52e9c30dd0283181a302e4655d4a79
zachlegros <legroszach3921@gmail.com>
2024-01-09 11:36:00 -0500

    Refactor checkout_sessions route to sync cart with DB and create/delete checkout sessions

--- app/api/checkout_sessions/route.ts
+++ app/api/checkout_sessions/route.ts
@@ -1,78 +1,179 @@
 import Stripe from "stripe";
 import {
+  Cart,
   CartItemType,
   CartProductType,
   getCartProductHardwareId,
   getCartProductMaterialId,
 } from "@/app/cart/store";
 import { syncCartWithComponents } from "@/app/cart/actions";
-import { getHardwareId, getMaterialId } from "@/utils/productUtils";
-import { cookies } from "next/headers";
+import {
+  CustomProductWithComponents,
+  DbProduct,
+  getHardwareId,
+  getMaterialId,
+  isCustomProductWithComponents,
+  isProduct,
+} from "@/utils/productUtils";
+import prisma from "@/lib/prisma";
+import { Product } from "@prisma/client";
+import { getLineItems, getTotalTps, getTotalTvq, inCents } from "../utils";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
+type FilteredCart = {
+  items: CartItemType<Product>[];
+  customItems: CartItemType<CustomProductWithComponents>[];
+};
+
+async function createCheckoutSessionInDB(params: {
+  sessionId: string;
+  cart: FilteredCart;
+  expiresAt: Date;
+  shippingId: string;
+}) {
+  try {
+    const { sessionId, cart, expiresAt, shippingId } = params;
+    await prisma.checkoutSession.create({
+      data: {
+        sid: sessionId,
+        expires_at: expiresAt,
+        items: {
+          create: cart.items.map((item) => ({
+            productId: item.product.id,
+            quantity: item.quantity,
+          })),
+        },
+        custom_items: {
+          create: cart.customItems.map((item) => ({
+            customProduct: {
+              create: {
+                name: item.product.name,
+                price: item.product.price,
+                quantity: item.quantity,
+                material_id: item.product.material_id,
+                hardware_id: item.product.hardware_id,
+              },
+            },
+            quantity: item.quantity,
+          })),
+        },
+        shipping_id: shippingId,
+      },
+      include: {
+        items: true,
+        custom_items: true,
+      },
+    });
+  } catch (err) {
+    throw new Error("failed_to_create_session_in_db");
+  }
+}
+
+export async function deleteCheckoutSessionInDB(checkoutSid: string) {
+  return prisma.checkoutSession.delete({
+    where: {
+      sid: checkoutSid,
+    },
+  });
+}
+
+function isCartInSync(cart: Cart, syncedCart: CartItemType<DbProduct>[]) {
+  return cart.every((item) =>
+    syncedCart.some(
+      (syncedItem) =>
+        item.product.id === syncedItem.product.id &&
+        getCartProductMaterialId(item.product) === getMaterialId(syncedItem.product) &&
+        getCartProductHardwareId(item.product) === getHardwareId(syncedItem.product)
+    )
+  );
+}
+
 export async function POST(req: Request) {
   try {
-    const cart = (await req.json()) as CartItemType<CartProductType>[];
+    const { cart, shippingId } = (await req.json()) as {
+      cart: CartItemType<CartProductType>[];
+      shippingId: string;
+    };
     if (!Array.isArray(cart) || !cart.length) throw new Error("cart_is_empty");
+    // Sync cart with DB
     const syncedCart = await syncCartWithComponents(cart);
-    const isCartInSync = cart.every((item) =>
-      syncedCart.some(
-        (syncedItem) =>
-          item.product.id === syncedItem.product.id &&
-          getCartProductMaterialId(item.product) === getMaterialId(syncedItem.product) &&
-          getCartProductHardwareId(item.product) === getHardwareId(syncedItem.product)
-      )
-    );
-    if (!isCartInSync) throw new Error("cart_out_of_sync");
+    const cartInSync = isCartInSync(cart, syncedCart);
+    if (!cartInSync) throw new Error("cart_out_of_sync");
 
-    const checkoutSid = cookies().get("checkout_sid")?.value;
-    if (checkoutSid) {
-      const session = await stripe.checkout.sessions.retrieve(checkoutSid);
-      if (session.payment_status === "paid") {
-        throw new Error("session_already_paid");
+    const filteredCart: FilteredCart = { items: [], customItems: [] };
+    for (const item of syncedCart) {
+      if (isProduct(item.product)) {
+        filteredCart.items.push({ product: item.product, quantity: item.quantity });
+      } else if (isCustomProductWithComponents(item.product)) {
+        filteredCart.customItems.push({ product: item.product, quantity: item.quantity });
       }
-      return new Response(JSON.stringify({ clientSecret: session.client_secret }), { status: 200 });
     }
 
-    const lineItems: Stripe.Checkout.SessionCreateParams.LineItem[] = syncedCart.map((item) => {
-      return {
-        price_data: {
-          currency: "cad",
-          product_data: {
-            name: item.product.name,
-          },
-          unit_amount: item.product.price * 100,
-        },
-        quantity: item.quantity,
-      };
-    });
+    // Create new checkout session
+    const shipping = await prisma.shipping.findUnique({ where: { id: shippingId } });
+    if (!shipping || isNaN(shipping.price)) throw new Error("shipping_invalid");
     const session = await stripe.checkout.sessions.create({
       ui_mode: "embedded",
       payment_method_types: ["card"],
       billing_address_collection: "required",
       expires_at: Math.floor(Date.now() / 1000) + 30 * 60,
-      line_items: lineItems,
+      line_items: [
+        ...getLineItems(syncedCart),
+        {
+          price_data: {
+            currency: "cad",
+            product_data: {
+              name: "TPS",
+            },
+            unit_amount: inCents(getTotalTps(syncedCart)),
+          },
+          quantity: 1,
+        },
+        {
+          price_data: {
+            currency: "cad",
+            product_data: {
+              name: "TVQ",
+            },
+            unit_amount: inCents(getTotalTvq(syncedCart)),
+          },
+          quantity: 1,
+        },
+        {
+          price_data: {
+            currency: "cad",
+            product_data: {
+              name: "Shipping",
+            },
+            unit_amount: inCents(shipping.price),
+          },
+          quantity: 1,
+        },
+      ],
       mode: "payment",
       return_url: `${req.headers.get("origin")}/checkout?session_id={CHECKOUT_SESSION_ID}`,
     });
-    // TODO: Save session.id to database alongside order details
+
     console.log("New checkout session:", session.id);
-    const expires = new Date(Date.now() + 60 * 30 * 1000).toUTCString();
-    return new Response(JSON.stringify({ clientSecret: session.client_secret }), {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json",
-        "Set-Cookie": `checkout_sid=${session.id}; httpOnly=true; expires=${expires}`,
-      },
+    const expiresAt = new Date(Date.now() + 60 * 30 * 1000);
+    // Save checkout session in DB
+    await createCheckoutSessionInDB({
+      sessionId: session.id,
+      cart: filteredCart,
+      expiresAt,
+      shippingId,
     });
+
+    return new Response(
+      JSON.stringify({ clientSecret: session.client_secret, sessionId: session.id }),
+      {
+        status: 200,
+      }
+    );
   } catch (err: any) {
     return new Response(JSON.stringify({ error: err.message }), {
       status: err.statusCode || 500,
-      headers: {
-        "Content-Type": "application/json",
-        "Set-Cookie": "checkout_sid=; httpOnly=true; expires=Thu, 01 Jan 1970 00:00:00 GMT",
-      },
     });
   }
 }
@@ -81,15 +182,41 @@ export async function GET(req: Request) {
   try {
     const { searchParams } = new URL(req.url);
     const sessionId = searchParams.get("session_id");
-    if (!sessionId) throw new Error("Missing session_id");
+    if (!sessionId) throw new Error("missing_session_id");
     const session = await stripe.checkout.sessions.retrieve(sessionId);
     return new Response(
       JSON.stringify({
         status: session.status,
-        customer_details: session.customer_details,
-      })
+      }),
+      {
+        status: 200,
+      }
     );
   } catch (err: any) {
-    return new Response(JSON.stringify({ error: err.message }), { status: err.statusCode || 500 });
+    return new Response(JSON.stringify({ error: err.message }), {
+      status: err.statusCode || 500,
+    });
+  }
+}
+
+export async function DELETE(req: Request) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const sessionId = searchParams.get("session_id");
+    if (!sessionId) throw new Error("missing_session_id");
+    const session = await stripe.checkout.sessions.expire(sessionId);
+    await deleteCheckoutSessionInDB(sessionId);
+    return new Response(
+      JSON.stringify({
+        status: session.status,
+      }),
+      {
+        status: 200,
+      }
+    );
+  } catch (err: any) {
+    return new Response(JSON.stringify({ error: err.message }), {
+      status: err.statusCode || 500,
+    });
   }
 }
---
+++ app/api/utils.ts
@@ -0,0 +1,42 @@
+import { DbProduct } from "@/utils/productUtils";
+import { CartItemType } from "../cart/store";
+import Stripe from "stripe";
+
+export function getTotalAmount(cart: CartItemType<DbProduct>[]) {
+  return cart.reduce((total, item) => {
+    return total + item.product.price * item.quantity;
+  }, 0);
+}
+
+export function getTotalTps(cart: CartItemType<DbProduct>[]) {
+  return 0.05 * getTotalAmount(cart);
+}
+
+export function getTotalTvq(cart: CartItemType<DbProduct>[]) {
+  return 0.09975 * getTotalAmount(cart);
+}
+
+export function inCents(price: number) {
+  return Math.round(price * 100);
+}
+
+export function orZero(value: number | null | undefined) {
+  return value ?? 0;
+}
+
+export function getLineItems(
+  cart: CartItemType<DbProduct>[]
+): Stripe.Checkout.SessionCreateParams.LineItem[] {
+  return cart.map((item) => {
+    return {
+      price_data: {
+        currency: "cad",
+        product_data: {
+          name: item.product.name,
+        },
+        unit_amount: inCents(item.product.price),
+      },
+      quantity: item.quantity,
+    };
+  });
+}
