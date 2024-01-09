88651b5d0088c19251d7e9585d156fff0c68cd13
zachlegros <legroszach3921@gmail.com>
2024-01-09 11:38:24 -0500

    Update product query to filter by quantity greater than 0

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -2,7 +2,7 @@ import prisma from "@/lib/prisma";
 import ProductsGrid from "./products";
 
 export default async function ProductsPage() {
-  const products = await prisma.product.findMany();
+  const products = await prisma.product.findMany({ where: { quantity: { gt: 0 } } });
 
   return <ProductsGrid products={products} />;
 }
