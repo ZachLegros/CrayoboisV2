0a266ebac2b53cbfff03c9813117358e787939e4
zachlegros <legroszach3921@gmail.com>
2024-01-10 18:36:10 -0500

    Added shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,70 +1,98 @@
-import { nextui } from "@nextui-org/react";
-import colors from "tailwindcss/colors";
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
-const scaleLight = {
-  ...colors.slate,
-  foreground: colors.slate[900],
-  DEFAULT: colors.slate[200],
-};
-
-const scaleDark = {
-  ...colors.slate,
-  foreground: colors.slate[100],
-  DEFAULT: colors.slate[800],
-};
-
-const config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        brand: {
+          DEFAULT: "hsl(var(--brand))",
+          foreground: "hsl(var(--brand-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        skeleton: {
+          DEFAULT: "hsl(var(--skeleton))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
       gridTemplateColumns: {
         "cart-item": "150px auto 125px 150px",
         "component-card-lg": "150px auto",
         "component-card-md": "125px auto",
         "component-card-sm": "100px auto",
       },
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
     },
   },
-  darkMode: "class",
-  plugins: [
-    nextui({
-      prefix: "nextui",
-      themes: {
-        light: {
-          extend: "light",
-          colors: {
-            primary: "#cc5500",
-            secondary: "#2E86AB",
-            background: "#ffffff",
-            foreground: "#000C14",
-            default: scaleLight,
-            content1: scaleLight,
-            content2: scaleLight,
-            content3: scaleLight,
-            content4: scaleLight,
-          }, // light theme colors
-        },
-        dark: {
-          extend: "dark",
-          colors: {
-            primary: "#cc5500",
-            secondary: "#2E86AB",
-            background: "#000C14",
-            foreground: "#ffffff",
-            default: scaleDark,
-            content1: scaleDark,
-            content2: scaleDark,
-            content3: scaleDark,
-            content4: scaleDark,
-          }, // dark theme colors
-        },
-      },
-    }),
-  ],
+  plugins: [require("tailwindcss-animate")],
 };
 
-export default config;
+// primary: "#cc5500",
+// secondary: "#2E86AB",
