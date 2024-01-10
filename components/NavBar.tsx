b9067d681e0ea9db38166fe9a1d3206f0d9ef575
zachlegros <legroszach3921@gmail.com>
2024-01-10 18:39:18 -0500

    Refactor NavBar component

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,16 +1,17 @@
 "use client";
 
-import { Suspense, useState } from "react";
-import { Navbar, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuToggle } from "@nextui-org/react";
+import { Suspense } from "react";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import AuthButton from "./AuthButton";
 import Logo from "./Logo";
-import ThemeSwitch from "./ThemeSwitch";
+import { ModeToggle } from "./ModeToggle";
+import Cart from "./Cart";
+
+export const NAVBAR_HEIGHT = 70;
 
 export default function NavBar() {
   const pathname = usePathname();
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const items = [
     {
@@ -28,69 +29,35 @@ export default function NavBar() {
   ];
 
   return (
-    <Navbar
-      maxWidth="xl"
-      onMenuOpenChange={setIsMenuOpen}
-      isBlurred={false}
-      isBordered
-      classNames={{
-        wrapper: "bg-background",
-        menu: "bg-background",
-      }}
-    >
-      <NavbarContent className="sm:hidden" justify="start">
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="sm:hidden"
-        />
-      </NavbarContent>
-      <NavbarContent className="sm:hidden" justify="center">
-        <Logo className="mr-4" />
-      </NavbarContent>
-      <NavbarContent className="hidden sm:flex gap-4" justify="start">
-        <Logo className="hidden sm:flex mr-4" />
-        {items.map((item, index) => {
-          const isActive = item.link === pathname;
-          return (
-            <Link
-              key={index}
-              href={item.link}
-              className={
-                isActive
-                  ? "text-md font-medium transition-colors hover:text-foreground"
-                  : "text-md font-sem text-foreground/70 transition-colors hover:text-foreground"
-              }
-            >
-              {item.title}
-            </Link>
-          );
-        })}
-      </NavbarContent>
-      <NavbarContent justify="end">
-        <NavbarItem className="flex">
-          <ThemeSwitch />
+    <header className="flex items-center sticky top-0 w-full h-16 z-50 border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/80">
+      <div className="flex justify-between items-center self-center w-full max-w-screen-xl mx-auto px-6">
+        <div className="flex gap-4 items-center">
+          <Logo className="hidden sm:flex mr-4" />
+          {items.map((item, index) => {
+            const isActive = item.link === pathname;
+            return (
+              <Link
+                key={index}
+                href={item.link}
+                className={
+                  isActive
+                    ? "text-md font-medium transition-colors hover:text-foreground"
+                    : "text-md font-sem text-foreground/70 transition-colors hover:text-foreground"
+                }
+              >
+                {item.title}
+              </Link>
+            );
+          })}
+        </div>
+        <div className="flex gap-2 items-center">
+          <ModeToggle />
+          <Cart />
           <Suspense>
             <AuthButton />
           </Suspense>
-        </NavbarItem>
-      </NavbarContent>
-      <NavbarMenu>
-        {items.map((item, index) => {
-          return (
-            <Link
-              key={index}
-              href={item.link}
-              className={
-                item.link === pathname
-                  ? "text-md font-medium transition-colors hover:text-foreground"
-                  : "text-md font-sem text-foreground/70 transition-colors hover:text-foreground"
-              }
-            >
-              {item.title}
-            </Link>
-          );
-        })}
-      </NavbarMenu>
-    </Navbar>
+        </div>
+      </div>
+    </header>
   );
 }
