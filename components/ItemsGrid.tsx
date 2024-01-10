e976742a0ec466e5fdcb614991744094def8701f
zachlegros <legroszach3921@gmail.com>
2024-01-10 18:43:14 -0500

    Refactor ItemsGrid component to use cn utility function

--- components/ItemsGrid.tsx
+++ components/ItemsGrid.tsx
@@ -1,10 +1,10 @@
+import { cn } from "@/lib/utils";
 import { ReactNode } from "react";
-import { twMerge } from "tailwind-merge";
 
 export default function ItemsGrid(props: { children: ReactNode; className?: string }) {
   const { children, className } = props;
   return (
-    <div className={twMerge("animate-in grid grid-cols-1 lg:grid-cols-2 gap-4", className)}>
+    <div className={cn("animate-in grid grid-cols-1 lg:grid-cols-2 gap-4", className)}>
       {children}
     </div>
   );
