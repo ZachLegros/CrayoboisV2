e663415c23020c5811f77cba2c54521d287c2180
zachlegros <legroszach3921@gmail.com>
2024-01-10 18:43:22 -0500

    Refactor Logo component to use Next.js Link instead of NextUILink

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Link as NextUILink } from "@nextui-org/react";
 import Link from "next/link";
 import Image from "next/image";
 import { useTheme } from "next-themes";
@@ -9,26 +8,23 @@ export default function Logo(props: { className?: string }) {
   const { className } = props;
   const { theme } = useTheme();
   return (
-    <NextUILink as={Link} href="/" className={className}>
-      {theme === "dark" ? (
-        <Image
-          src="/images/logo-light.svg"
-          width={100}
-          height={50}
-          className="min-w-[100px]"
-          alt="crayobois-logo"
-          priority={true}
-        />
-      ) : (
-        <Image
-          src="/images/logo-dark.svg"
-          width={100}
-          height={50}
-          className="min-w-[100px]"
-          alt="crayobois-logo"
-          priority={true}
-        />
-      )}
-    </NextUILink>
+    <Link href="/" className={className}>
+      <Image
+        src="/images/logo-light.svg"
+        width={100}
+        height={50}
+        className={`min-w-[100px] ${theme === "dark" ? "visible" : "hidden"}`}
+        alt="crayobois-logo"
+        loading="lazy"
+      />
+      <Image
+        src="/images/logo-dark.svg"
+        width={100}
+        height={50}
+        className={`min-w-[100px] ${theme === "dark" ? "hidden" : "visible"}`}
+        alt="crayobois-logo"
+        loading="lazy"
+      />
+    </Link>
   );
 }
