d12664509a495dc450c922ad567c2e9712b2b0c0
zachlegros <legroszach3921@gmail.com>
2024-01-10 02:26:57 -0500

    Add dark mode support to Logo component

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,19 +3,32 @@
 import { Link as NextUILink } from "@nextui-org/react";
 import Link from "next/link";
 import Image from "next/image";
+import { useTheme } from "next-themes";
 
 export default function Logo(props: { className?: string }) {
   const { className } = props;
+  const { theme } = useTheme();
   return (
     <NextUILink as={Link} href="/" className={className}>
-      <Image
-        src="/images/logo-light.svg"
-        width={100}
-        height={50}
-        className="min-w-[100px]"
-        alt="crayobois-logo"
-        priority={true}
-      />
+      {theme === "dark" ? (
+        <Image
+          src="/images/logo-light.svg"
+          width={100}
+          height={50}
+          className="min-w-[100px]"
+          alt="crayobois-logo"
+          priority={true}
+        />
+      ) : (
+        <Image
+          src="/images/logo-dark.svg"
+          width={100}
+          height={50}
+          className="min-w-[100px]"
+          alt="crayobois-logo"
+          priority={true}
+        />
+      )}
     </NextUILink>
   );
 }
