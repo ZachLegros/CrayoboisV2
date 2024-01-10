244fcb5b03a3af8b718651004187f5f830be88ac
zachlegros <legroszach3921@gmail.com>
2024-01-10 18:38:25 -0500

    Refactor providers and add utils module

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,16 +1,13 @@
 "use client";
 
-import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import NextTopLoader from "nextjs-toploader";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="light">
-      <NextUIProvider>
-        <NextTopLoader color="#cc5500" showSpinner={false} />
-        {children}
-      </NextUIProvider>
+    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+      <NextTopLoader color="#cc5500" showSpinner={false} />
+      {children}
     </NextThemesProvider>
   );
 }
