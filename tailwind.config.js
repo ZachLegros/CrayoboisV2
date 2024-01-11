8079b1f3f40b05d36d47778c98b474c69072c858
zachlegros <legroszach3921@gmail.com>
2024-01-11 15:12:34 -0500

    Add new background-content color to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,7 @@ module.exports = {
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
+        "background-content": "hsl(var(--background-content))",
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
