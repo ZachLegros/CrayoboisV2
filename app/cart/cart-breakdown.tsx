16d21a086e13b2e986ac6580d00a550b39cef999
zachlegros <legroszach3921@gmail.com>
2024-01-11 15:15:49 -0500

    Add Card component to CartBreakdown

--- app/cart/cart-breakdown.tsx
+++ app/cart/cart-breakdown.tsx
@@ -9,6 +9,7 @@ import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
+import { Card } from "@/components/ui/card";
 
 export default function CartBreakdown() {
   const router = useRouter();
@@ -39,7 +40,7 @@ export default function CartBreakdown() {
   }, [shippingMethods]);
 
   return (
-    <div className="flex flex-col gap-2 w-full text-l">
+    <Card className="flex flex-col gap-2 w-full text-lg p-3 dark:bg-background dark:border-none dark:shadow-none">
       <div className="flex flex-col gap-2 mb-2">
         <p className="font-semibold">Méthode de livraison</p>
         <p>
@@ -48,28 +49,30 @@ export default function CartBreakdown() {
           )} et plus ou
           l'achat d'au moins 4 produits.`}
         </p>
-        {nonFreeShippingMethods.length > 0 ? (
-          shippingMethod?.price === 0 ? (
-            <Badge variant="default-faded">Livraison gratuite!</Badge>
+        <div className="min-h-12">
+          {nonFreeShippingMethods.length > 0 ? (
+            shippingMethod?.price === 0 ? (
+              <Badge variant="default-faded">Livraison gratuite!</Badge>
+            ) : (
+              <RadioGroup onValueChange={(id: string) => setShippingMethod(id)}>
+                {nonFreeShippingMethods.map((method, index) => (
+                  <div className="flex items-center space-x-2" key={index}>
+                    <RadioGroupItem
+                      checked={shippingMethod?.id === method.id}
+                      value={`${method.id}`}
+                      id={method.id}
+                    />
+                    <Label htmlFor={method.id} className="cursor-pointer">
+                      {method.name} ({cad(method.price)})
+                    </Label>
+                  </div>
+                ))}
+              </RadioGroup>
+            )
           ) : (
-            <RadioGroup onValueChange={(id: string) => setShippingMethod(id)}>
-              {nonFreeShippingMethods.map((method, index) => (
-                <div className="flex items-center space-x-2" key={index}>
-                  <RadioGroupItem
-                    checked={shippingMethod?.id === method.id}
-                    value={`${method.id}`}
-                    id={method.id}
-                  />
-                  <Label htmlFor={method.id} className="cursor-pointer">
-                    {method.name} ({cad(method.price)})
-                  </Label>
-                </div>
-              ))}
-            </RadioGroup>
-          )
-        ) : (
-          <Skeleton className="w-full h-14" />
-        )}
+            <Skeleton className="w-full h-12" />
+          )}
+        </div>
       </div>
       <div className="flex justify-between mt-2">
         <span>Sous-total</span>
@@ -107,6 +110,6 @@ export default function CartBreakdown() {
       >
         Commander
       </Button>
-    </div>
+    </Card>
   );
 }
