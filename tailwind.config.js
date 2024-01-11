ecb1a13e84c00951aaf86205fb7d7188f6791940
zachlegros <legroszach3921@gmail.com>
2024-01-11 01:43:40 -0500

    Add new font sizes to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,10 @@ module.exports = {
       },
     },
     extend: {
+      fontSize: {
+        "2xs": "0.625rem",
+        "3xs": "0.5rem",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
