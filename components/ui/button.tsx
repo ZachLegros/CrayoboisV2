130e58ec09aed8cfe9cd901859ffe54571414e9b
zachlegros <legroszach3921@gmail.com>
2024-01-11 15:14:44 -0500

    Remove unused button variant

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -11,8 +11,6 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default: "bg-primary text-primary-foreground shadow hover:bg-primary/90",
-        "default-faded":
-          "bg-primary-faded text-primary-faded-foreground shadow-sm hover:bg-primary-faded/90",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         "destructive-faded":
