5a59ee758cfb3cd0a422d202f0f52883ff9c027c
zachlegros <legroszach3921@gmail.com>
2024-01-11 00:40:19 -0500

    Refactor CartPage component to pass referer pathname to Cart component

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,5 +1,13 @@
 import Cart from "./cart";
+import { headers } from "next/headers";
 
 export default async function CartPage() {
-  return <Cart />;
+  const headersList = headers();
+  const referer = headersList.get("referer");
+
+  if (referer === null) return <Cart />;
+
+  const refererPathname = new URL(referer).pathname;
+
+  return <Cart refererPathname={refererPathname} />;
 }
