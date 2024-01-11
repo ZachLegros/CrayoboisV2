957fd6ca9470d9a3d583dd7db67c140e06261186
zachlegros <legroszach3921@gmail.com>
2024-01-11 01:43:48 -0500

    Refactor Cart component to display cart badge

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -2,12 +2,13 @@
 
 import { useRouter } from "next/navigation";
 import { FaShoppingCart } from "react-icons/fa";
-// import { useCartStore } from "@/app/cart/store";
+import { useCartStore } from "@/app/cart/store";
 import { Button } from "./ui/button";
+import { Badge } from "./ui/badge";
 
 export default function Cart() {
   const router = useRouter();
-  // const { cart } = useCartStore();
+  const { cart } = useCartStore();
 
   return (
     <Button
@@ -17,15 +18,14 @@ export default function Cart() {
       aria-label="cart"
       onClick={() => router.push("/cart")}
     >
-      <FaShoppingCart className="text-2xl" />
-      {/* <Badge
-        content={cart.length}
-        color="primary"
-        size="sm"
-        isInvisible={cart.length < 1}
-        showOutline={false}
-      >
-      </Badge> */}
+      <div className="relative">
+        {cart.length > 0 && (
+          <Badge className="flex justify-center items-center absolute top-0 right-0 -mt-1.5 -mr-1.5 rounded-full p-0 text-2xs w-3.5 h-3.5">
+            {cart.length}
+          </Badge>
+        )}
+        <FaShoppingCart className="text-xl" />
+      </div>
     </Button>
   );
 }
