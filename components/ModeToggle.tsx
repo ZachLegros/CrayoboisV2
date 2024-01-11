976891c3a794a980942d82877c3e19da64c6e3b7
zachlegros <legroszach3921@gmail.com>
2024-01-11 16:44:50 -0500

    Refactor ModeToggle component to support alignment and add French translations

--- components/ModeToggle.tsx
+++ components/ModeToggle.tsx
@@ -11,24 +11,34 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { cn } from "@/lib/utils";
 
-export function ModeToggle() {
-  const { setTheme } = useTheme();
-
+export function ModeToggle(props: {
+  align?: "center" | "end" | "start";
+  className?: string;
+}) {
+  const { theme, setTheme } = useTheme();
+  const { align = "end", className } = props;
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
+        <Button variant="outline" className={cn("p-2", className)}>
+          <span className="inline-flex mr-2 lg:hidden lg:mr-0">Thème</span>
+          <SunIcon
+            aria-hidden={theme === "dark"}
+            className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 aria-hidden:hidden"
+          />
+          <MoonIcon
+            aria-hidden={theme === "light"}
+            className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 aria-hidden:hidden"
+          />
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>Light</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>Dark</DropdownMenuItem>
+      <DropdownMenuContent align={align}>
+        <DropdownMenuItem onClick={() => setTheme("light")}>Clair</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>Sombre</DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
+          Système
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
