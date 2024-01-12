53da361741d93a705569b5cfe80e93e16845554c
zachlegros <legroszach3921@gmail.com>
2024-01-12 02:05:16 -0500

    Refactor layout component to use grid system and add floating filter trigger

--- app/custom-order/layout.tsx
+++ app/custom-order/layout.tsx
@@ -1,13 +1,16 @@
 import { ReactNode } from "react";
 import SidePanel from "./side-panel";
+import FloatingFilterTrigger from "@/components/FloatingFilterTrigger";
 
 export default function Layout(props: { children: ReactNode }) {
   const { children } = props;
-
   return (
-    <div className="flex w-full gap-4">
-      <SidePanel />
-      <div className="flex-1">{children}</div>
+    <div className="grid grid-cols-1 md:grid-cols-12 gap-4">
+      <SidePanel className="hidden md:flex col-span-1 md:col-span-4 lg:col-span-3" />
+      <div className="flex flex-col col-span-1 md:col-span-8 lg:col-span-9 gap-4 h-[calc(100vh - 4rem - 1.5rem - 1px)]">
+        {children}
+      </div>
+      <FloatingFilterTrigger className="flex md:hidden" />
     </div>
   );
 }
