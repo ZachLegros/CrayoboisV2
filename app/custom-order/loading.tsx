4a84dcd3acff38b8a2761c0886d50f5be60e916b
zachlegros <legroszach3921@gmail.com>
2024-01-12 02:04:43 -0500

    Refactor loading component and update Breadcrumbs import

--- app/custom-order/loading.tsx
+++ app/custom-order/loading.tsx
@@ -1,4 +1,5 @@
 // import { Breadcrumbs } from "@/components/Breadcrumbs";
+import { Breadcrumbs } from "@/components/Breadcrumbs";
 import ItemsGrid from "@/components/ItemsGrid";
 import { Skeleton } from "@/components/ui/skeleton";
 
@@ -6,13 +7,17 @@ export default function Loading() {
   return (
     <div className="flex w-full h-full gap-4">
       <div className="flex flex-col w-full gap-4">
-        {/* <Breadcrumbs
+        <Breadcrumbs
+          className="hidden sm:flex"
           steps={["Choix du bois", "Choix du matériel", "Ajouter au panier"]}
           currentStep={0}
-        /> */}
+        />
         <ItemsGrid className="w-full h-full">
           {[...Array(12).keys()].map((_, index) => (
-            <Skeleton className="w-full h-[174px] p-3" key={index} />
+            <Skeleton
+              className="w-full h-[120px] sm:h-[129px] md:h-[154px] lg:h-[179px] p-3"
+              key={index}
+            />
           ))}
         </ItemsGrid>
       </div>
