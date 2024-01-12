a35b09d92e29d8748b3f68b0c737fba9df782a71
zachlegros <legroszach3921@gmail.com>
2024-01-12 02:03:47 -0500

    Refactor ProductCard component and add ExpandImage component

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -9,38 +9,13 @@ import { Product } from "@prisma/client";
 import { useToast } from "@/components/ui/use-toast";
 import { Card } from "./ui/card";
 import { Button } from "./ui/button";
-import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
-// import { Tooltip } from "./ui/tooltip";
-
-export function ExpandImage(props: {
-  isExpanded?: boolean;
-  src: string;
-  alt: string;
-}) {
-  const { isExpanded, src, alt } = props;
-  return (
-    <Dialog open={isExpanded}>
-      <DialogTrigger>
-        <Button
-          variant="ghost"
-          size="icon"
-          className="absolute bottom-0 right-0 m-1"
-        >
-          <FaExpandAlt className="text-lg" />
-        </Button>
-      </DialogTrigger>
-      <DialogContent>
-        <ImageWithLoading
-          src={src}
-          alt={alt}
-          width={500}
-          height={500}
-          quality={80}
-        />
-      </DialogContent>
-    </Dialog>
-  );
-}
+import { Dialog, DialogContent } from "./ui/dialog";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
 
 export default function ProductCard(props: {
   product: Product;
@@ -50,7 +25,7 @@ export default function ProductCard(props: {
   const { toast } = useToast();
   const { addToCart, isProductInCart } = useCartStore();
   const [isInCart, setIsInCart] = useState(isProductInCart(product));
-  const [, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleAddToCart = () => {
     addToCart(product);
@@ -59,34 +34,83 @@ export default function ProductCard(props: {
   };
 
   return (
-    <Card className="flex flex-col gap-4 p-3">
-      <div className="relative">
-        <ImageWithLoading
-          width={225}
-          height={225}
-          src={product.image}
-          alt={product.name}
-          className="rounded-md cursor-pointer"
-          quality={70}
-          onClick={() => setIsOpen(true)}
-        />
-        {/* <ExpandImage isExpanded={isOpen} src={product.image} alt={product.name} /> */}
-      </div>
-      <div className="flex flex-col gap-2 flex-1">
-        <div className="flex flex-col gap-2 text-lg">
-          <b>{product.name}</b>
-          <p>{cad(product.price)}</p>
+    <>
+      <Card className="flex flex-col gap-4 p-3">
+        <div className="relative">
+          <ImageWithLoading
+            width={225}
+            height={225}
+            src={product.image}
+            alt={product.name}
+            className="rounded-md cursor-pointer"
+            quality={70}
+            onClick={() => setIsOpen(true)}
+          />
+          <div className="absolute bottom-0 right-0 m-1 ">
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  <Button
+                    variant="secondary"
+                    size="icon"
+                    className="opacity-60"
+                    onClick={() => setIsOpen(true)}
+                  >
+                    <FaExpandAlt className="text-lg" />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent className="bg-foreground text-background">
+                  Agrandir
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </div>
         </div>
-        {!isInCart ? (
-          <Button color="primary" className="mt-auto" onClick={handleAddToCart}>
-            Ajouter au Panier
-          </Button>
-        ) : (
-          <Button variant="outline" className="mt-auto" disabled>
-            Dans le Panier
-          </Button>
-        )}
-      </div>
-    </Card>
+        <div className="flex flex-col gap-2 flex-1">
+          <div className="flex flex-col gap-2 text-lg">
+            <b>{product.name}</b>
+            <p>{cad(product.price)}</p>
+          </div>
+          {!isInCart ? (
+            <Button color="primary" className="mt-auto" onClick={handleAddToCart}>
+              Ajouter au Panier
+            </Button>
+          ) : (
+            <Button variant="outline" className="mt-auto" disabled>
+              Dans le Panier
+            </Button>
+          )}
+        </div>
+      </Card>
+      <ExpandImage
+        isOpen={isOpen}
+        onOpenChange={setIsOpen}
+        src={product.image}
+        alt={product.name}
+      />
+    </>
+  );
+}
+
+export function ExpandImage(props: {
+  isOpen?: boolean;
+  onOpenChange?: (isOpen: boolean) => void;
+  src: string;
+  alt: string;
+}) {
+  const { isOpen, onOpenChange, src, alt } = props;
+  return (
+    <Dialog open={isOpen} onOpenChange={onOpenChange}>
+      <DialogContent className="max-w-[500px] max-h-[500px] p-0 rounded-xl overflow-hidden">
+        <ImageWithLoading
+          src={src}
+          alt={alt}
+          width={500}
+          height={500}
+          className="w-full h-full object-contain"
+          quality={90}
+        />
+      </DialogContent>
+    </Dialog>
   );
 }
