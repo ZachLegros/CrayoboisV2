ef531293f8c99d636e7a191e2d6ccdcd4b8f60a7
zachlegros <legroszach3921@gmail.com>
2024-01-13 15:06:10 -0500

    Refactor Breadcrumbs component to improve readability and maintainability

--- components/Breadcrumbs.tsx
+++ components/Breadcrumbs.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { cn } from "@/lib/utils";
 import { FaChevronRight as ChevronRight } from "react-icons/fa";
 
 export function Breadcrumbs(props: {
@@ -12,43 +11,32 @@ export function Breadcrumbs(props: {
   const { steps, currentStep, onAction, className } = props;
 
   return (
-    <ol
-      className={cn(
-        "bg-card border h-8 w-max px-3 flex items-center rounded-md",
-        className
-      )}
-    >
-      {steps.map((step, index) => (
-        <li key={index}>
-          <div className="relative flex items-center">
-            {index !== 0 && (
-              <div
-                className="flex-shrink-0 w-9 rounded-full flex items-center justify-center bg-primary-600 text-foreground"
-                aria-hidden="true"
-              >
-                <span className="mx-2 opacity-50">
-                  <ChevronRight className="h-4 w-4" />
-                </span>
-              </div>
-            )}
-            <div className="min-w-0 flex-1">
-              <div
-                className="text-foreground text-sm font-medium data-[previous=true]:underline data-[previous=true]:cursor-pointer data-[next=true]:opacity-50 truncate"
-                aria-current={index === currentStep ? "page" : undefined}
-                data-previous={index < currentStep}
-                data-next={index > currentStep}
-                onClick={() => {
-                  if (index < currentStep && onAction) {
-                    onAction(index);
-                  }
-                }}
-              >
-                {step}
-              </div>
-            </div>
-          </div>
-        </li>
-      ))}
+    <ol className={className}>
+      {steps.map((step, index) => {
+        const separator = index !== 0 && (
+          <li className="mx-2 opacity-50">
+            <ChevronRight className="h-4 w-4" />
+          </li>
+        );
+        return (
+          <>
+            {separator}
+            <li
+              className="text-foreground text-sm font-medium data-[previous=true]:underline data-[previous=true]:cursor-pointer data-[next=true]:opacity-50 truncate"
+              aria-current={index === currentStep ? "page" : undefined}
+              data-previous={index < currentStep}
+              data-next={index > currentStep}
+              onClick={() => {
+                if (index < currentStep && onAction) {
+                  onAction(index);
+                }
+              }}
+            >
+              {step}
+            </li>
+          </>
+        );
+      })}
     </ol>
   );
 }
