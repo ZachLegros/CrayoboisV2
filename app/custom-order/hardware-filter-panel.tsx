b7a62294fb495e02bf08f7dac200470fb4ca9474
zachlegros <legroszach3921@gmail.com>
2024-01-13 15:04:51 -0500

    Refactor filter panels to use separate component files

--- app/custom-order/hardware-filter-panel.tsx
+++ app/custom-order/hardware-filter-panel.tsx
@@ -1,20 +1,12 @@
 "use client";
 
-import React, { useMemo } from "react";
 import { useCustomOrderStore } from "./store";
-import Filter from "../../components/Filter";
 import { ResetButton } from "./material-filter-panel";
+import HardwareFilters from "./hardware-filters";
 
 export default function HardwareFilterPanel(props: { isDisabled?: boolean }) {
   const { isDisabled } = props;
-  const { hardwares, typeFilter, priceFilter } = useCustomOrderStore();
-
-  const hardwareTypes: { [type: string]: number } = useMemo(() => {
-    return hardwares.reduce((acc, hardware) => {
-      acc[hardware.name] = acc[hardware.name] ? acc[hardware.name] + 1 : 1;
-      return acc;
-    }, {} as any);
-  }, [hardwares]);
+  const { typeFilter, priceFilter } = useCustomOrderStore();
 
   const handleReset = () => {
     typeFilter.setEnabled(true);
@@ -35,32 +27,7 @@ export default function HardwareFilterPanel(props: { isDisabled?: boolean }) {
           onClick={handleReset}
         />
       </div>
-      <Filter
-        filterName="Par type"
-        filterValues={[
-          { value: "all", label: "Tous les types", amount: hardwares.length },
-          ...Object.keys(hardwareTypes).map((type) => ({
-            value: type,
-            label: type,
-            amount: hardwareTypes[type],
-          })),
-        ]}
-        currentValue={typeFilter.value}
-        setValue={typeFilter.setValue}
-        filterEnabled={typeFilter.enabled}
-        setFilterEnabled={typeFilter.setEnabled}
-      />
-      <Filter
-        filterName="Par prix"
-        filterValues={[
-          { value: "desc", label: "Prix descendant" },
-          { value: "asc", label: "Prix ascendant" },
-        ]}
-        currentValue={priceFilter.value}
-        setValue={priceFilter.setValue}
-        filterEnabled={priceFilter.enabled}
-        setFilterEnabled={priceFilter.setEnabled}
-      />
+      <HardwareFilters />
     </div>
   );
 }
--- app/custom-order/material-filter-panel.tsx
+++ app/custom-order/material-filter-panel.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import React, { useMemo } from "react";
 import { useCustomOrderStore } from "./store";
-import Filter from "../../components/Filter";
 import { Button, ButtonProps } from "@/components/ui/button";
+import MaterialFilters from "./material-filters";
 
 export function ResetButton(props: ButtonProps) {
   return (
@@ -14,21 +13,7 @@ export function ResetButton(props: ButtonProps) {
 }
 
 export default function MaterialFilterPanel() {
-  const { materials, originFilter, typeFilter, priceFilter } = useCustomOrderStore();
-
-  const materialTypes: { [type: string]: number } = useMemo(() => {
-    return materials.reduce((acc, material) => {
-      acc[material.type] = acc[material.type] ? acc[material.type] + 1 : 1;
-      return acc;
-    }, {} as any);
-  }, [materials]);
-
-  const materialOrigins = useMemo(() => {
-    return materials.reduce((acc, material) => {
-      acc[material.origin] = acc[material.origin] ? acc[material.origin] + 1 : 1;
-      return acc;
-    }, {} as any);
-  }, [materials]);
+  const { originFilter, typeFilter, priceFilter } = useCustomOrderStore();
 
   const handleReset = () => {
     typeFilter.setEnabled(true);
@@ -49,47 +34,7 @@ export default function MaterialFilterPanel() {
           onClick={handleReset}
         />
       </div>
-      <Filter
-        filterName="Par type"
-        filterValues={[
-          { value: "all", label: "Tous les types", amount: materials.length },
-          ...Object.keys(materialTypes).map((type) => ({
-            value: type,
-            label: type,
-            amount: materialTypes[type],
-          })),
-        ]}
-        currentValue={typeFilter.value}
-        setValue={typeFilter.setValue}
-        filterEnabled={typeFilter.enabled}
-        setFilterEnabled={typeFilter.setEnabled}
-      />
-      <Filter
-        filterName="Par prix"
-        filterValues={[
-          { value: "desc", label: "Prix descendant" },
-          { value: "asc", label: "Prix ascendant" },
-        ]}
-        currentValue={priceFilter.value}
-        setValue={priceFilter.setValue}
-        filterEnabled={priceFilter.enabled}
-        setFilterEnabled={priceFilter.setEnabled}
-      />
-      <Filter
-        filterName="Par origine"
-        filterValues={[
-          { value: "all", label: "Toutes les origines", amount: materials.length },
-          ...Object.keys(materialOrigins).map((origin) => ({
-            value: origin,
-            label: origin,
-            amount: materialOrigins[origin],
-          })),
-        ]}
-        currentValue={originFilter.value}
-        setValue={originFilter.setValue}
-        filterEnabled={originFilter.enabled}
-        setFilterEnabled={originFilter.setEnabled}
-      />
+      <MaterialFilters />
     </div>
   );
 }
