1b21d30a88955b65b6285f75456595b7cdcea673
zachlegros <legroszach3921@gmail.com>
2024-01-13 15:06:00 -0500

    Add FloatingBar component

---
+++ components/FloatingBar.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { type ReactNode } from "react";
+import { cn } from "@/lib/utils";
+import { Portal } from "@radix-ui/react-portal";
+
+export default function FloatingBar(props: {
+  children: ReactNode;
+  className?: string;
+}) {
+  const { className, children } = props;
+
+  return (
+    <Portal>
+      <div
+        className={cn(
+          "flex w-full items-end fixed bottom-0 left-0 z-50 h-32 bg-gradient-to-t from-background pointer-events-none",
+          className
+        )}
+      >
+        {children}
+      </div>
+    </Portal>
+  );
+}
