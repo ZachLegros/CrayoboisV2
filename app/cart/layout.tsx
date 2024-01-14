115c775427b7f12bb714c784c131519ebdc7aa21
zachlegros <legroszach3921@gmail.com>
2024-01-13 20:29:15 -0500

    Update cart layout

--- app/cart/layout.tsx
+++ app/cart/layout.tsx
@@ -5,8 +5,8 @@ export default function Layout(props: { children: React.ReactNode }) {
 
   return (
     <div className="flex flex-col">
-      <div className="flex items-center space-x-2 h-8 mb-8 px-3">
-        <FaShoppingCart className="text-2xl" />
+      <div className="flex items-center space-x-2 h-8 mb-4 md:mb-8">
+        <FaShoppingCart className="text-xl md:text-2xl" />
         <p className="text-2xl font-semibold">Mon panier</p>
       </div>
       <div className="flex w-full h-full space-x-11">{children}</div>
