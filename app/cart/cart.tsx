8ef31e8e937d59cd1dc3b7f66db2b0702cef45b7
zachlegros <legroszach3921@gmail.com>
2024-01-13 20:29:27 -0500

    Update Cart component styling

--- app/cart/cart.tsx
+++ app/cart/cart.tsx
@@ -20,14 +20,14 @@ export default function Cart() {
 
   return (
     <>
-      <Card className="animate-in p-3 h-max space-y-4 dark:bg-background dark:border-none dark:shadow-none">
+      <Card className="animate-in w-full h-max space-y-4 dark:bg-background dark:border-none dark:shadow-none">
         {cart.toReversed().map((item, index) => (
           <div className="flex flex-col gap-4" key={index}>
             <CartItem item={item} hasSeparator={index !== cart.length - 1} />
           </div>
         ))}
       </Card>
-      <div className="min-w-80 max-w-80 h-max flex-grow sticky top-0 -mt-[calc(4rem+1.5rem+1px)] pt-[calc(1.5rem+2rem+2rem+1px)]">
+      <div className="hidden lg:flex min-w-80 max-w-80 h-max flex-grow sticky top-0 -mt-[calc(4rem+1.5rem+1px)] pt-[calc(1.5rem+2rem+2rem+1px)]">
         <CartBreakdown />
       </div>
     </>
