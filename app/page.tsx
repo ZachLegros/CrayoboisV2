65385d0ba799ca79072701b3a006b4283794f0ea
zachlegros <legroszach3921@gmail.com>
2024-01-17 22:58:34 -0500

    Refactor Quebec component to be a local function

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,7 @@ export default function Index() {
   );
 }
 
-export function Quebec(props: { className?: string }) {
+function Quebec(props: { className?: string }) {
   const { className } = props;
   return (
     <svg
