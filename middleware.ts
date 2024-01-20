cbe6cd64aa03b1a5b4ea33864e8665f421a9c4c1
zachlegros <legroszach3921@gmail.com>
2024-01-20 14:18:51 -0500

    Add admin role check in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,31 @@
 import { NextResponse, type NextRequest } from "next/server";
 import { createClient } from "@/lib/supabase/middleware";
+import { Role } from "@prisma/client";
 
 export async function middleware(request: NextRequest) {
   const { supabase, response } = createClient(request);
+
   const { data } = await supabase.auth.getSession();
 
-  if (data?.session && request.nextUrl.pathname.startsWith("/login")) {
+  if (request.nextUrl.pathname.startsWith("/admin")) {
+    try {
+      const userId = data?.session?.user.id;
+      if (!userId) throw new Error("missing_user_id");
+      const userData: { role: Role } = await fetch(
+        `${request.nextUrl.origin}/api/user_role?user_id=${userId}`
+      ).then((res) => res.json());
+      if (userData.role !== "admin")
+        return NextResponse.redirect(new URL("/", request.url));
+    } catch {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
+
+  if (data.session && request.nextUrl.pathname.startsWith("/login")) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
-  if (!data?.session && request.nextUrl.pathname.startsWith("/orders")) {
+  if (!data.session && request.nextUrl.pathname.startsWith("/orders")) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
