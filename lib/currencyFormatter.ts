e2d238faaa3d6cc02937becb5d4a7d28a8153e08
zachlegros <legroszach3921@gmail.com>
2024-01-21 14:10:00 -0500

    Add cadPrecision function to currencyFormatter

--- lib/currencyFormatter.ts
+++ lib/currencyFormatter.ts
@@ -7,3 +7,7 @@ const formatter = new Intl.NumberFormat("en-US", {
 export const cad = (amount: number) => {
   return formatter.format(amount);
 };
+
+export const cadPrecision = (amount: number) => {
+  return parseFloat(amount.toFixed(2));
+};
