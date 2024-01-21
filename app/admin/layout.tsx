5d398fb7748076b479af463f94607c9f37cb1fa3
zachlegros <legroszach3921@gmail.com>
2024-01-21 14:12:11 -0500

    Add admin layout component

---
+++ app/admin/layout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+import SidePanel from "./side-panel";
+
+export default function Layout(props: { children: ReactNode }) {
+  const { children } = props;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-admin-panel gap-8">
+      <SidePanel className="hidden md:flex" />
+      <div className="flex flex-col gap-4">{children}</div>
+    </div>
+  );
+}
