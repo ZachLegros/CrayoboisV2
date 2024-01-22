466a7b83de4cb21b58f532e53167dd9e1aa4b063
zachlegros <legroszach3921@gmail.com>
2024-01-21 23:48:13 -0500

    Refactor loading spinner in Chart component

--- app/admin/chart.tsx
+++ app/admin/chart.tsx
@@ -3,16 +3,15 @@
 import type { Props as ApexChartProps } from "react-apexcharts";
 import dynamic from "next/dynamic";
 import { Spinner } from "@/components/ui/spinner";
-import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Chart(props: ApexChartProps) {
   const { ...chartProps } = props;
   const ApexChart = dynamic(() => import("react-apexcharts"), {
     ssr: false,
     loading: () => (
-      <Skeleton className="flex justify-center items-center w-full h-full rounded-none">
+      <div className="flex justify-center items-center w-full h-full rounded-none">
         <Spinner />
-      </Skeleton>
+      </div>
     ),
   });
 
