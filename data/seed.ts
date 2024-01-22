16bd31e31cf58ff413bf60f8cd4e0cefeb336d77
zachlegros <legroszach3921@gmail.com>
2024-01-21 23:48:33 -0500

    Add custom product creation and retrieval in client order creation

--- data/seed.ts
+++ data/seed.ts
@@ -53,25 +53,48 @@ try {
   const createOrderPromises: any[] = [];
   for (const orderData of orders) {
     const { custom_products, ...order } = orderData;
+    await prisma.customProduct.createMany({
+      data: custom_products.map((custom_product) => ({
+        id: custom_product.id,
+        name: custom_product.name,
+        quantity: custom_product.quantity,
+        price: custom_product.price,
+        material_id: custom_product.material_id,
+        hardware_id: custom_product.hardware_id,
+      })),
+    });
+    const customProducts = await prisma.customProduct.findMany({
+      where: {
+        id: {
+          in: custom_products.map((custom_product) => custom_product.id),
+        },
+      },
+      include: {
+        material: {
+          select: {
+            id: true,
+            name: true,
+            price: true,
+            image: true,
+          },
+        },
+        hardware: {
+          select: {
+            id: true,
+            name: true,
+            price: true,
+            image: true,
+          },
+        },
+      },
+    });
+
     const createQuery = prisma.clientOrder.create({
       data: {
         ...order,
         status: order.status as OrderStatus,
         user: undefined,
-        custom_products: {
-          create: custom_products.map((custom_product) => ({
-            id: custom_product.id,
-            name: custom_product.name,
-            quantity: custom_product.quantity,
-            price: custom_product.price,
-            material: {
-              connect: { id: custom_product.material_id },
-            },
-            hardware: {
-              connect: { id: custom_product.hardware_id },
-            },
-          })),
-        },
+        custom_products: customProducts,
       },
     });
     createOrderPromises.push(createQuery);
