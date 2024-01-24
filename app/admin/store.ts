eb80ba12ff82f27e949ec3a6b1686cb9be9c19aa
zachlegros <legroszach3921@gmail.com>
2024-01-23 22:14:16 -0500

    Add updateOrderStatus function to AdminStore

--- app/admin/store.ts
+++ app/admin/store.ts
@@ -1,14 +1,23 @@
 import { create } from "zustand";
-import { ClientOrder } from "@prisma/client";
+import { ClientOrder, OrderStatus } from "@prisma/client";
 
 type AdminStore = {
   orders: ClientOrder[];
   setOrders: (orders: ClientOrder[]) => void;
+  updateOrderStatus: (orderId: string, orderStatus: OrderStatus) => void;
 };
 
-export const useAdminStore = create<AdminStore>((set) => ({
+export const useAdminStore = create<AdminStore>((set, get) => ({
   orders: [],
   setOrders: (orders: ClientOrder[]) => set({ orders }),
+  updateOrderStatus: (orderId: string, orderStatus: OrderStatus) => {
+    const orderIndex = get().orders.findIndex((order) => order.id === orderId);
+    if (orderIndex !== -1) {
+      const orders = [...get().orders];
+      orders[orderIndex] = { ...orders[orderIndex], status: orderStatus };
+      set({ orders });
+    }
+  },
 }));
 
 export default useAdminStore;
