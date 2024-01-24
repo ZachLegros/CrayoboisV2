cc46734fcbe2675b79d939b160c58a52714d60f4
zachlegros <legroszach3921@gmail.com>
2024-01-23 22:15:57 -0500

    Remove back button from order layout

--- app/admin/orders/[id]/layout.tsx
+++ app/admin/orders/[id]/layout.tsx
@@ -1,23 +1,8 @@
 "use client";
 
 import type { ReactNode } from "react";
-import { Button } from "@/components/ui/button";
-import { FaChevronLeft } from "react-icons/fa";
-import { useRouter } from "next/navigation";
 
 export default function Layout(props: { children: ReactNode }) {
   const { children } = props;
-  const router = useRouter();
-
-  return (
-    <div className="flex flex-col gap-3">
-      <div>
-        <Button onClick={() => router.push("/admin/orders")}>
-          <FaChevronLeft className="mr-1" />
-          Commandes
-        </Button>
-      </div>
-      {children}
-    </div>
-  );
+  return <div className="flex flex-col gap-3">{children}</div>;
 }
