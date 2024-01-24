7badc417f711952b06ffa599e1c544ce3e982717
zachlegros <legroszach3921@gmail.com>
2024-01-23 22:15:39 -0500

    Add setOrderStatusInDb function to admin/orders/actions.ts

---
+++ app/admin/orders/actions.ts
@@ -0,0 +1,24 @@
+"use server";
+
+import { createClient } from "@/lib/supabase/server";
+import { OrderStatus } from "@prisma/client";
+import { cookies } from "next/headers";
+import prisma from "@/lib/prisma";
+
+export async function setOrderStatusInDb(orderId: string, orderStatus: OrderStatus) {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  const isAdmin = session?.user.user_metadata.role === "admin";
+  if (isAdmin) {
+    await prisma.clientOrder.update({
+      where: { id: orderId },
+      data: { status: orderStatus },
+    });
+    return true;
+  }
+  return false;
+}
