2d68eae590a498864dc0975fb7f260fc0414dff6
zachlegros <legroszach3921@gmail.com>
2024-01-24 23:17:12 -0500

    Add updateMaterialInDb function to handle material updates

---
+++ app/admin/materials/actions.ts
@@ -0,0 +1,25 @@
+"use server";
+
+import { createClient } from "@/lib/supabase/server";
+import { cookies } from "next/headers";
+import prisma from "@/lib/prisma";
+import { Material } from "@prisma/client";
+
+export async function updateMaterialInDb(material: Material) {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  const isAdmin = session?.user.user_metadata.role === "admin";
+
+  if (isAdmin) {
+    await prisma.material.update({
+      where: { id: material.id },
+      data: { ...material },
+    });
+    return true;
+  }
+  return false;
+}
