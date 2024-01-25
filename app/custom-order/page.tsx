05823932450875f646144227fe94607bd7e84c92
zachlegros <legroszach3921@gmail.com>
2024-01-24 23:17:28 -0500

    Refactor API calls in CustomOrderPage

--- app/custom-order/page.tsx
+++ app/custom-order/page.tsx
@@ -3,8 +3,8 @@ import OrderBuilder from "./order-builder";
 
 export default async function CustomOrderPage() {
   const [materials, hardwares] = await Promise.all([
-    prisma.material.findMany({ where: { enabled: true } }),
-    prisma.hardware.findMany({ where: { enabled: true } }),
+    prisma.material.findMany({ where: { enabled: { equals: true } } }),
+    prisma.hardware.findMany({ where: { enabled: { equals: true } } }),
   ]);
 
   return <OrderBuilder materials={materials} hardwares={hardwares} />;
