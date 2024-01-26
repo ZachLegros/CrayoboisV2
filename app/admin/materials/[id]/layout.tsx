77dc0bbfefa4e56124d97028d031d0d511a1e0a7
zachlegros <legroszach3921@gmail.com>
2024-01-25 21:39:01 -0500

    Add layout component for materials

---
+++ app/admin/materials/[id]/layout.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import type { ReactNode } from "react";
+
+export default function Layout(props: { children: ReactNode }) {
+  const { children } = props;
+  return <div className="flex flex-col gap-3">{children}</div>;
+}
