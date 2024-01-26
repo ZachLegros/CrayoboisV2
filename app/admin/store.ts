09dca92bb6e75bfc4a3c537671b3b68aa812a07a
zachlegros <legroszach3921@gmail.com>
2024-01-25 21:38:53 -0500

    Refactor admin store to use material ID as key

--- app/admin/store.ts
+++ app/admin/store.ts
@@ -1,13 +1,21 @@
+"use client";
+
 import { create } from "zustand";
 import { ClientOrder, Material, OrderStatus } from "@prisma/client";
+import { updateMaterialInDb } from "./materials/actions";
 
 type AdminStore = {
   orders: ClientOrder[];
   setOrders: (orders: ClientOrder[]) => void;
   updateOrderStatus: (orderId: string, orderStatus: OrderStatus) => void;
-  materials: Material[];
+  materials: { [key: string]: Material };
+  setMaterial: (material: Material) => void;
   setMaterials: (materials: Material[]) => void;
-  updateMaterial: (material: Material) => void;
+  updateMaterial: <P extends keyof Material>(
+    material: Material,
+    property: P,
+    value: Material[P]
+  ) => Promise<boolean>;
 };
 
 export const useAdminStore = create<AdminStore>((set, get) => ({
@@ -22,17 +30,31 @@ export const useAdminStore = create<AdminStore>((set, get) => ({
     }
   },
 
-  materials: [],
-  setMaterials: (materials: Material[]) => set({ materials }),
-  updateMaterial: (material: Material) => {
-    const materialIndex = get().materials.findIndex(
-      (m) => m.id === material.id
-    );
-    if (materialIndex !== -1) {
-      const materials = [...get().materials];
-      materials[materialIndex] = material;
-      set({ materials });
-    }
+  materials: {},
+  setMaterial: (material: Material) => {
+    set({ materials: { ...get().materials, [material.id]: material } });
+  },
+  setMaterials: (materials: Material[]) => {
+    const materialsObj = materials.reduce((acc, material) => {
+      return { ...acc, [material.id]: material };
+    }, {});
+    set({ materials: materialsObj });
+  },
+  updateMaterial: async <P extends keyof Material>(
+    material: Material,
+    property: P,
+    value: Material[P]
+  ) => {
+    const originalMaterials = { ...get().materials };
+    const updatedMaterial = { ...material, [property]: value };
+    const newMaterials = { ...get().materials, [material.id]: updatedMaterial };
+    set({ materials: newMaterials });
+    return updateMaterialInDb(updatedMaterial).then((success) => {
+      if (!success) {
+        set({ materials: originalMaterials });
+      }
+      return success;
+    });
   },
 }));
 
