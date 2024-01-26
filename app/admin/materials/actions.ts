6653e9cba1e693e1877a7c164b6fef533b5de5c4
zachlegros <legroszach3921@gmail.com>
2024-01-25 21:38:44 -0500

    Refactor getMaterials function to include admin role check

--- app/admin/materials/actions.ts
+++ app/admin/materials/actions.ts
@@ -5,9 +5,19 @@ import { cookies } from "next/headers";
 import prisma from "@/lib/prisma";
 import { Material } from "@prisma/client";
 
-export async function getMaterials() {
-  const materials = await prisma.material.findMany();
-  return materials;
+export async function getMaterials(): Promise<Material[]> {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  const isAdmin = session?.user.user_metadata.role === "admin";
+
+  if (isAdmin) {
+    return await prisma.material.findMany();
+  }
+  return [];
 }
 
 export async function updateMaterialInDb(material: Material) {
