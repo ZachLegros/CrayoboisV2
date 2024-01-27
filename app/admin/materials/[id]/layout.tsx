7082291a99a988154c207d6c6cce955fdf225369
zachlegros <legroszach3921@gmail.com>
2024-01-26 23:07:22 -0500

    Refactor layout component to include MaterialHeader

--- app/admin/materials/[id]/layout.tsx
+++ app/admin/materials/[id]/layout.tsx
@@ -1,8 +1,14 @@
 "use client";
 
 import type { ReactNode } from "react";
+import MaterialHeader from "./material-header";
 
 export default function Layout(props: { children: ReactNode }) {
   const { children } = props;
-  return <div className="flex flex-col gap-3">{children}</div>;
+  return (
+    <div className="flex flex-col gap-3 p-3 border rounded-xl">
+      <MaterialHeader />
+      {children}
+    </div>
+  );
 }
