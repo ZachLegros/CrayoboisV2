8f8a3c95c7b7694951317107fd292d6410247a54
zachlegros <legroszach3921@gmail.com>
2024-01-27 10:57:12 -0500

    Refactor MaterialDetails component

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,9 +1,8 @@
-import React from "react";
 import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-import ASCIIFolder from "fold-to-ascii";
 import dayjslib from "dayjs";
 import "dayjs/locale/fr-ca";
+import React from "react";
+import { twMerge } from "tailwind-merge";
 
 dayjslib.locale("fr-ca");
 export const dayjs = dayjslib;
@@ -14,39 +13,11 @@ export function cn(...inputs: ClassValue[]) {
 
 export function getValidChildren(children: React.ReactNode) {
   return React.Children.toArray(children).filter((child) =>
-    React.isValidElement(child)
+    React.isValidElement(child),
   ) as React.ReactElement[];
 }
 
-export async function sequentialAsyncOperations<T>(
-  values: T[],
-  asyncOperation: (value: T) => Promise<any>
-) {
-  try {
-    const result = await values.reduce(
-      async (previousPromise: Promise<any>, currentValue: T) => {
-        const currentResult = await asyncOperation(currentValue);
-        await previousPromise;
-        return [...(await previousPromise), currentResult];
-      },
-      Promise.resolve([])
-    );
-    return result;
-  } catch (error) {
-    console.error("Error:", error);
-  }
-}
-
-export const toSnakeCase = (str: string) => {
-  const ascii = ASCIIFolder.foldReplacing(str);
-  const groups = ascii.match(
-    /[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g
-  );
-  if (groups === null) return;
-  return groups.map((x) => x.toLowerCase()).join("_");
-};
-
-export function shuffleArray(array: any[]) {
+export function shuffleArray<T>(array: T[]) {
   const newArray = [...array];
   for (let i = newArray.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -66,7 +37,7 @@ export function getTvq(amount: number) {
 export function map(
   value: number,
   oldRange: [number, number],
-  newRange: [number, number]
+  newRange: [number, number],
 ) {
   const newValue =
     ((value - oldRange[0]) * (newRange[1] - newRange[0])) /
