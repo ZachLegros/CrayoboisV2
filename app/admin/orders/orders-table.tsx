f0585bf1e6ae0874eed9878f9693393dbb5bcca4
zachlegros <legroszach3921@gmail.com>
2024-01-27 17:08:30 -0500

    Add useMemo hook to improve performance in OrdersTable component

--- app/admin/orders/orders-table.tsx
+++ app/admin/orders/orders-table.tsx
@@ -12,13 +12,33 @@ import { cad } from "@/lib/currencyFormatter";
 import { dayjs } from "@/lib/utils";
 import { ClientOrder, OrderStatus } from "@prisma/client";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import useAdminStore from "../store";
 
 export default function OrdersTable(props: { orders: ClientOrder[] }) {
   const { orders: ordersFromDb } = props;
   const router = useRouter();
   const { orders, setOrders } = useAdminStore();
+
+  const tableItems = useMemo(() => {
+    console.log(orders);
+    return Object.keys(orders).map((orderId) => {
+      const order = orders[orderId];
+      return (
+        <TableRow
+          className="h-12 cursor-pointer"
+          key={order.id}
+          onClick={() => router.push(`/admin/orders/${order.id}`)}
+        >
+          <TableCell className="font-medium">#{order.order_no}</TableCell>
+          <TableCell>{dayjs(order.created_at).format("D MMM YYYY")}</TableCell>
+          <TableCell className="capitalize">{order.payer_name}</TableCell>
+          <TableCell>{cad(order.amount)}</TableCell>
+          <TableCell className="text-right">{orderStatus(order.status)}</TableCell>
+        </TableRow>
+      );
+    });
+  }, [orders]);
 
   useEffect(() => {
     if (Object.keys(orders).length === 0) setOrders(ordersFromDb);
@@ -35,26 +55,7 @@ export default function OrdersTable(props: { orders: ClientOrder[] }) {
           <TableHead className="text-right">Statut</TableHead>
         </TableRow>
       </TableHeader>
-      <TableBody>
-        {Object.keys(orders).map((orderId) => {
-          const order = orders[orderId];
-          return (
-            <TableRow
-              className="h-12 cursor-pointer"
-              key={order.id}
-              onClick={() => router.push(`/admin/orders/${order.id}`)}
-            >
-              <TableCell className="font-medium">#{order.order_no}</TableCell>
-              <TableCell>{dayjs(order.created_at).format("D MMM YYYY")}</TableCell>
-              <TableCell className="capitalize">{order.payer_name}</TableCell>
-              <TableCell>{cad(order.amount)}</TableCell>
-              <TableCell className="text-right">
-                {orderStatus(order.status)}
-              </TableCell>
-            </TableRow>
-          );
-        })}
-      </TableBody>
+      <TableBody>{tableItems}</TableBody>
     </Table>
   );
 }
