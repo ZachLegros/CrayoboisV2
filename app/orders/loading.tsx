aa2905785c67cfc4838578e0d4a43bef387df8e3
zachlegros <legroszach3921@gmail.com>
2024-01-28 00:22:18 -0500

    Refactor Loading component to center spinner

--- app/orders/loading.tsx
+++ app/orders/loading.tsx
@@ -1,5 +1,9 @@
 import { Spinner } from "@/components/ui/spinner";
 
 export default function Loading() {
-  return <Spinner className="text-primary w-10 h-10" />;
+  return (
+    <div className="flex flex-auto items-center justify-center">
+      <Spinner className="text-primary w-10 h-10" />
+    </div>
+  );
 }
