2973dfbe9aee16775d5e2d11525a9917d0d59c9c
zachlegros <legroszach3921@gmail.com>
2024-01-27 22:21:42 -0500

    Refactor login page layout

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "./login-form";
 
 export default async function LoginPage() {
   return (
-    <div className="flex items-center justify-center h-[calc(100vh-64px-1.5rem)] md:h-[calc(100vh-64px-3rem)]">
+    <div className="flex items-center justify-center flex-auto">
       <LoginForm />
     </div>
   );
