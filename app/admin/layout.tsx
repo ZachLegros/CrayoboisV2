513acbfff7a3ddd51179c740642d8f4debb2d1cf
zachlegros <legroszach3921@gmail.com>
2024-01-28 13:27:18 -0500

    Update admin layout and add loading component

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout(props: { children: ReactNode }) {
   const { children } = props;
 
   return (
-    <div className="bg-background grid shadow-lg md:p-3 md:border rounded-xl grid-cols-1 md:grid-cols-admin-panel gap-8">
+    <div className="flex-auto bg-background grid shadow-lg md:p-3 md:border rounded-xl grid-cols-1 md:grid-cols-admin-panel gap-8">
       <SidePanel className="hidden md:flex" />
       <div className="flex flex-col gap-4">{children}</div>
     </div>
