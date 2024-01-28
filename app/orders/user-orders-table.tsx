82efb3be4e1da3c3d7b1c998745e9d9045ab39ee
zachlegros <legroszach3921@gmail.com>
2024-01-28 00:22:22 -0500

    Refactor UserOrdersTable component

--- app/orders/user-orders-table.tsx
+++ app/orders/user-orders-table.tsx
@@ -6,15 +6,65 @@ import {
   TableBody,
   TableCell,
   TableHead,
+  TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { dayjs } from "@/lib/utils";
-import type { ClientOrder } from "@prisma/client";
-import Link from "next/link";
+import { cad } from "@/lib/currencyFormatter";
+import { useMediaQuery } from "@/lib/hooks";
+import { gtSm } from "@/lib/mediaQueries";
+import type { CustomProductWithComponents } from "@/lib/productUtils";
+import { dayjs, orderStatus } from "@/lib/utils";
+import type { ClientOrder, Product } from "@prisma/client";
 import { useRouter } from "next/navigation";
+import { useMemo } from "react";
 
 export default function UserOrdersTable({ orders }: { orders: ClientOrder[] }) {
   const router = useRouter();
+  const isDesktop = useMediaQuery(gtSm);
+
+  const tableItems = useMemo(() => {
+    return orders.map((order) => {
+      let products: Product[] = [];
+      let customProducts: CustomProductWithComponents[] = [];
+      if (order.products) products = order.products as Product[];
+      if (order.custom_products)
+        customProducts = order.custom_products as CustomProductWithComponents[];
+      const items = [...products, ...customProducts].map((product) => product.name);
+      return (
+        <TableRow
+          className="h-12 cursor-pointer"
+          onClick={() => router.push(`/orders/${order.id}`)}
+          key={order.id}
+        >
+          <TableCell>
+            <Button variant="link" className="underline text-primary font-semibold">
+              #{order.order_no}
+            </Button>
+          </TableCell>
+          <TableCell>{dayjs(order.created_at).format("D MMM YYYY")}</TableCell>
+          {isDesktop && (
+            <TableCell className="flex flex-col">
+              <ul>
+                {items.map((item, index) => (
+                  <li
+                    className="whitespace-nowrap overflow-hidden text-ellipsis max-w-[300px]"
+                    key={item}
+                  >
+                    {items.length > 1 && (
+                      <span className="font-semibold ">{index + 1}.</span>
+                    )}{" "}
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </TableCell>
+          )}
+          <TableCell>{cad(order.amount)}</TableCell>
+          <TableCell className="text-right">{orderStatus(order.status)}</TableCell>
+        </TableRow>
+      );
+    });
+  }, [orders, isDesktop]);
 
   if (!orders.length) {
     return (
@@ -33,31 +83,17 @@ export default function UserOrdersTable({ orders }: { orders: ClientOrder[] }) {
   }
 
   return (
-    <div>
-      <h3 className="text-xl font-semibold">Mes commandes</h3>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Order ID</TableCell>
-            <TableCell>Amount</TableCell>
-            <TableCell>Status</TableCell>
-            <TableCell>Created</TableCell>
-            <TableCell>Updated</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {orders.map((order) => (
-            <TableRow key={order.id}>
-              <TableCell>
-                <Link href={`/orders/${order.id}`}>{order.id}</Link>
-              </TableCell>
-              <TableCell>{order.amount}</TableCell>
-              <TableCell>{order.status}</TableCell>
-              <TableCell>{dayjs(order.created_at).format("DD MMM YYYY")}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </div>
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Num. de commande</TableHead>
+          <TableHead>Date</TableHead>
+          {isDesktop && <TableHead>Items</TableHead>}
+          <TableHead>Total</TableHead>
+          <TableHead className="text-right">Statut</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>{tableItems}</TableBody>
+    </Table>
   );
 }
