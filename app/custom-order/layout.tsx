b5dd25edf153fce8166e963bbf44a280f19c8966
zachlegros <legroszach3921@gmail.com>
2024-01-28 22:37:43 -0500

    Refactor layout and add flex-auto to container elements

--- app/custom-order/layout.tsx
+++ app/custom-order/layout.tsx
@@ -4,9 +4,9 @@ import SidePanel from "./side-panel";
 export default function Layout(props: { children: ReactNode }) {
   const { children } = props;
   return (
-    <div className="grid grid-cols-1 md:grid-cols-12 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-12 gap-4 flex-auto">
       <SidePanel className="hidden md:flex col-span-1 md:col-span-4 lg:col-span-3" />
-      <div className="flex flex-col col-span-1 md:col-span-8 lg:col-span-9 gap-4 ">
+      <div className="flex flex-col col-span-1 md:col-span-8 lg:col-span-9 gap-4 flex-auto">
         {children}
       </div>
     </div>
