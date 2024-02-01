fbbacbcee1c434e5fdd9625691d69b3f589b8da9
zachlegros <legroszach3921@gmail.com>
2024-01-31 21:55:55 -0500

    Replace Skeleton with Spinner in Loading component

--- app/admin/loading.tsx
+++ app/admin/loading.tsx
@@ -1,5 +1,9 @@
-import { Skeleton } from "@/components/ui/skeleton";
+import { Spinner } from "@/components/ui/spinner";
 
 export default function Loading() {
-  return <Skeleton className="animate-in flex-auto" />;
+  return (
+    <div className="animate-in flex flex-auto justify-center items-center md:p-3 md:border rounded-xl">
+      <Spinner className="text-primary size-10" />
+    </div>
+  );
 }
