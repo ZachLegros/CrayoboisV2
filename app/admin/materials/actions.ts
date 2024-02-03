2012b37cb96bc91fbc88d3c24acfcb7eee28a68f
zachlegros <legroszach3921@gmail.com>
2024-02-02 23:00:41 -0500

    Add new material creation functionality

--- app/admin/materials/actions.ts
+++ app/admin/materials/actions.ts
@@ -2,8 +2,11 @@
 
 import prisma from "@/lib/prisma";
 import { createClient } from "@/lib/supabase/server";
+import { createNoCookiesClient } from "@/lib/supabase/serverNoCookies";
 import type { Material } from "@prisma/client";
+import snakeCase from "lodash.snakecase";
 import { cookies } from "next/headers";
+import sharp from "sharp";
 
 export async function getMaterials(): Promise<Material[]> {
   const cookieStore = cookies();
@@ -37,4 +40,70 @@ export async function updateMaterialInDb(material: Material) {
     return true;
   }
   return false;
+}
+
+export async function addNewMaterial({
+  name,
+  price,
+  image,
+  origin,
+  type,
+}: {
+  name: string;
+  price: number;
+  image: string;
+  origin: string;
+  type: string;
+}) {
+  try {
+    const cookieStore = cookies();
+    let supabase = createClient(cookieStore);
+
+    const {
+      data: { session },
+    } = await supabase.auth.getSession();
+    const isAdmin = session?.user.user_metadata.role === "admin";
+    if (!isAdmin) throw new Error("Not admin");
+
+    const binaryImage = Buffer.from(image, "base64");
+    // resize image
+    const jpg = await sharp(binaryImage)
+      .resize({ width: 256, height: 256, fit: "cover" })
+      .jpeg({ quality: 75 })
+      .toBuffer();
+
+    // create client with service role
+    supabase = createNoCookiesClient();
+    // upload image to supabase
+    const { data, error } = await supabase.storage
+      .from("inventory")
+      .upload(`materials/${snakeCase(name)}.jpg`, jpg, {
+        upsert: false,
+        contentType: "image/jpg",
+      });
+
+    if (error) {
+      throw error;
+    }
+
+    console.log(
+      `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/inventory/${data.path}`,
+    );
+
+    // await prisma.material.create({
+    //   data: {
+    //     name,
+    //     price,
+    //     origin,
+    //     type,
+    //     nature: "bois",
+    //     image: imageUrl,
+    //   },
+    // });
+
+    return true;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
 }
---
+++ app/admin/materials/create-material.tsx
@@ -0,0 +1,170 @@
+"use client";
+
+import ImageInput from "@/components/ImageInput";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { getBase64 } from "@/lib/utils";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { addNewMaterial } from "./actions";
+
+type FormValues = {
+  name: string;
+  type: string;
+  origin: string;
+  price: number;
+  image: string;
+  quantity: number;
+};
+
+export default function CreateMaterial(props: { children: React.ReactNode }) {
+  const { children } = props;
+  const { reset, resetField, setValue, control, handleSubmit, formState } =
+    useForm<FormValues>({
+      defaultValues: {
+        name: "",
+        type: "",
+        origin: "",
+        price: 0,
+        image: "",
+        quantity: 0,
+      },
+    });
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setLoading(true);
+      const success = await addNewMaterial({ ...data });
+      if (success) {
+        setOpen(false);
+        reset();
+      }
+    } catch (error) {
+      console.error(error);
+    }
+    setLoading(false);
+  });
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader className="text-xl font-semibold">
+          Ajouter un matériau
+        </DialogHeader>
+        <DialogDescription>
+          <form onSubmit={onSubmit}>
+            <div className="flex flex-col sm:flex-row gap-3">
+              <div className="flex-auto">
+                <Controller
+                  name="image"
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <ImageInput
+                      {...field}
+                      onClear={() => resetField("image")}
+                      onChange={async (event) => {
+                        if (!event.target.files) return;
+                        const b64 = await getBase64(event.target.files[0]);
+                        if (typeof b64 === "string")
+                          setValue("image", b64, { shouldValidate: true });
+                      }}
+                    />
+                  )}
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <Controller
+                  name="name"
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <div>
+                      <Label htmlFor="name">Nom</Label>
+                      <Input {...field} placeholder="Nom" />
+                    </div>
+                  )}
+                />
+                <Controller
+                  name="type"
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <div>
+                      <Label htmlFor="type">Type</Label>
+                      <Input {...field} placeholder="Type" />
+                    </div>
+                  )}
+                />
+                <Controller
+                  name="origin"
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <div>
+                      <Label htmlFor="origin">Origine</Label>
+                      <Input {...field} placeholder="Origine" />
+                    </div>
+                  )}
+                />
+                <Controller
+                  name="price"
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <div>
+                      <Label htmlFor="price">Prix</Label>
+                      <Input {...field} type="number" placeholder="Prix" min={0} />
+                    </div>
+                  )}
+                />
+                <Controller
+                  name="quantity"
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <div>
+                      <Label htmlFor="quantity">Quantité</Label>
+                      <Input
+                        {...field}
+                        type="number"
+                        placeholder="Quantité"
+                        min={0}
+                      />
+                    </div>
+                  )}
+                />
+              </div>
+            </div>
+            <div className="flex h-max w-max items-center ml-auto mt-3">
+              <DialogClose asChild>
+                <Button type="button" variant="secondary" className="mr-2">
+                  Annuler
+                </Button>
+              </DialogClose>
+              <Button
+                type="submit"
+                isLoading={loading}
+                disabled={!formState.isValid || loading}
+              >
+                Ajouter
+              </Button>
+            </div>
+          </form>
+        </DialogDescription>
+      </DialogContent>
+    </Dialog>
+  );
+}
