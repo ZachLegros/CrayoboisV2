52f114cd04afcce54fcb42c6eea2883a81a09c34
zachlegros <legroszach3921@gmail.com>
2024-02-02 23:23:20 -0500

    Add quantity field to addNewMaterial function

--- app/admin/materials/actions.ts
+++ app/admin/materials/actions.ts
@@ -45,12 +45,14 @@ export async function updateMaterialInDb(material: Material) {
 export async function addNewMaterial({
   name,
   price,
+  quantity,
   image,
   origin,
   type,
 }: {
   name: string;
   price: number;
+  quantity: number;
   image: string;
   origin: string;
   type: string;
@@ -78,7 +80,7 @@ export async function addNewMaterial({
     const { data, error } = await supabase.storage
       .from("inventory")
       .upload(`materials/${snakeCase(name)}.jpg`, jpg, {
-        upsert: false,
+        upsert: true,
         contentType: "image/jpg",
       });
 
@@ -86,20 +88,21 @@ export async function addNewMaterial({
       throw error;
     }
 
-    console.log(
-      `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/inventory/${data.path}`,
-    );
+    const url = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/inventory/${data.path}`;
 
-    // await prisma.material.create({
-    //   data: {
-    //     name,
-    //     price,
-    //     origin,
-    //     type,
-    //     nature: "bois",
-    //     image: imageUrl,
-    //   },
-    // });
+    console.log("New material image url:", url);
+
+    await prisma.material.create({
+      data: {
+        name,
+        price: parseFloat(price.toFixed(2)),
+        quantity: quantity,
+        origin,
+        type,
+        nature: "bois",
+        image: url,
+      },
+    });
 
     return true;
   } catch (error) {
