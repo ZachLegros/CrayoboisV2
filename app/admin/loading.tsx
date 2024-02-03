ae2910149fca7a2c7618ad82714c1bc7cae974c6
zachlegros <legroszach3921@gmail.com>
2024-02-03 12:05:09 -0500

    Refactor admin layout and loading components

--- app/admin/loading.tsx
+++ app/admin/loading.tsx
@@ -2,7 +2,7 @@ import { Spinner } from "@/components/ui/spinner";
 
 export default function Loading() {
   return (
-    <div className="animate-in flex flex-auto justify-center items-center md:p-3 md:border rounded-xl">
+    <div className="animate-in flex flex-auto justify-center items-center">
       <Spinner className="text-primary size-10" />
     </div>
   );
