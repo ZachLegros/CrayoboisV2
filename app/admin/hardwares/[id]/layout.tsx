766fbc0776e2618637eac264326170c767d2e990
zachlegros <legroszach3921@gmail.com>
2024-02-03 12:05:34 -0500

    Refactor hardware layout and table

--- app/admin/hardwares/[id]/layout.tsx
+++ app/admin/hardwares/[id]/layout.tsx
@@ -6,7 +6,7 @@ import HardwareHeader from "./hardware-header";
 export default function Layout(props: { children: ReactNode }) {
   const { children } = props;
   return (
-    <div className="flex flex-col gap-3 p-3 border rounded-xl">
+    <div className="flex flex-col flex-auto gap-3">
       <HardwareHeader />
       {children}
     </div>
