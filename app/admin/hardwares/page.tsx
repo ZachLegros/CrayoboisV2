abdb6b80a5e788de47eeb7b35b5a8d2ca89f5498
zachlegros <legroszach3921@gmail.com>
2024-02-02 23:53:18 -0500

    Add create hardware functionality and button

--- app/admin/hardwares/page.tsx
+++ app/admin/hardwares/page.tsx
@@ -1,13 +1,22 @@
 export const dynamic = "force-dynamic";
 
+import { Button } from "@/components/ui/button";
 import prisma from "@/lib/prisma";
+import { FaPlus } from "react-icons/fa";
+import CreateHardware from "./create-hardware";
 import HardwaresTable from "./hardwares-table";
 
 export default async function AdminMaterials() {
   const hardwares = await prisma.hardware.findMany();
 
   return (
-    <div className="md:p-3 md:border rounded-xl">
+    <div className="flex flex-col md:p-3 md:border rounded-xl gap-3">
+      <CreateHardware>
+        <Button className="ml-auto">
+          Ajouter du matériel
+          <FaPlus className="ml-1" />
+        </Button>
+      </CreateHardware>
       <HardwaresTable hardwares={hardwares} />
     </div>
   );
