93f9b624b5c4ff102f6d57c471800a5cdd57134f
zachlegros <legroszach3921@gmail.com>
2024-02-03 14:08:39 -0500

    Refactor ImageInput component to handle image resizing and display

--- components/ImageInput.tsx
+++ components/ImageInput.tsx
@@ -1,43 +1,56 @@
 "use client";
 
 import { Input, type InputProps } from "@/components/ui/input";
+import { getBase64 } from "@/lib/utils";
 import { Cross1Icon } from "@radix-ui/react-icons";
+import reduce from "image-blob-reduce";
 import Image from "next/image";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { FaFileImage } from "react-icons/fa";
 import { AspectRatio } from "./ui/aspect-ratio";
 import { Button } from "./ui/button";
 
 export default function ImageInput(
-  props: InputProps & { onClear?: () => void; aspectRatio?: number },
+  props: Omit<InputProps, "onChange"> & {
+    onChange?: (b64Image: string) => void;
+    onClear?: () => void;
+    aspectRatio?: number;
+  },
 ) {
   const { onClear, aspectRatio = 1, ...otherProps } = props;
-  const [file, setFile] = useState<File | null>(null);
+  const [image, setImage] = useState<string | null>(null);
 
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files) {
-      setFile(event.target.files[0]);
-    }
-    props.onChange?.(event);
-  };
+  const handleFileChange = useCallback(
+    async (event: React.ChangeEvent<HTMLInputElement>) => {
+      if (reduce && event.target.files) {
+        const resizedImage = await reduce().toBlob(event.target.files[0], {
+          max: 1024,
+        });
+        const b64 = await getBase64(resizedImage);
+        setImage(b64);
+        props.onChange?.(b64);
+      }
+    },
+    [],
+  );
 
   return (
     <div className="flex">
-      {file ? (
+      {image ? (
         <AspectRatio ratio={aspectRatio} className="relative">
           <Button
             size="icon"
             variant="secondary"
             className="absolute size-6 right-0 m-1 z-10 opacity-80"
             onClick={() => {
-              setFile(null);
+              setImage(null);
               onClear?.();
             }}
           >
             <Cross1Icon className="size-3" />
           </Button>
           <Image
-            src={URL.createObjectURL(file)}
+            src={image}
             width={200}
             height={200}
             alt="product image"
